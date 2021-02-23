f8a4149ef6a4596332c76f94fa19842ba8b29942
WildWuzzy <jtj.lord@gmail.com>
2021-02-23 13:54:05 +0100

    Small modifications to landing and _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
+import '../styles/theme.css'
 import type { AppProps } from 'next/app'
+import Header from '../app/components/Header/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
