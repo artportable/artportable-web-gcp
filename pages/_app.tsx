ba1826949c47368cb4eb56ba7e8e718714fd20ed
WildWuzzy <jtj.lord@gmail.com>
2021-02-23 15:14:31 +0100

    Add shadow and height to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '../styles/theme.css'
+import '../styles/variables.css'
 import type { AppProps } from 'next/app'
 import Header from '../app/components/Header/Header'
 
