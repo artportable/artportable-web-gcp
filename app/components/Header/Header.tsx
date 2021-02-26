eaccfd27afb1774c645331ea52e95fb095f79266
WildWuzzy <jtj.lord@gmail.com>
2021-02-26 14:34:26 +0100

    Add i18n base imports and examples

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,35 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useTranslation } from 'next-i18next'
 import s from './header.module.css'
 
 export default function Header() {
-    return (
-        <div className={s.container}>
-            <div className={s.logo}>
-                <Link href="/">
-                    <a>
-                        <Image
-                            src="/art-logo-TEMP.PNG"
-                            alt="Logo Artportable"
-                            width={188}
-                            height={51}
-                        />
-                    </a>
-                </Link>
-            </div>
-            <div className={s.navigation}>
-                <Link href="/discover">
-                    <a>Upptäck</a>
-                </Link>
-                <Link href="/about">
-                    <a>Om oss</a>
-                </Link>
-            </div>
-            <div className={s.language}>
-                Swe
-                <FontAwesomeIcon icon="chevron-down" />
-            </div>
-        </div>
-    );
+  const { t } = useTranslation('common');
+
+  return (
+    <div className={s.container}>
+      <div className={s.logo}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/art-logo-TEMP.PNG"
+              alt="Logo Artportable"
+              width={188}
+              height={51}
+            />
+          </a>
+        </Link>
+      </div>
+      <div className={s.navigation}>
+        <Link href="/discover">
+          <a>{t('discover')}</a>
+        </Link>
+        <Link href="/about">
+          <a>Om oss</a>
+        </Link>
+      </div>
+      <div className={s.language}>
+        Swe
+        <FontAwesomeIcon icon="chevron-down" />
+      </div>
+    </div>
+  );
 }
