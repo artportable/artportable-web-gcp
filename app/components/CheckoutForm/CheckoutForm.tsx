bcc0b3d9df2b0594943ad6b3310aa4bf87895208
Anders <andersroseen@gmail.com>
2021-03-08 16:09:05 +0100

    Add card element and create a Subscription

---
+++ app/components/CheckoutForm/CheckoutForm.tsx
@@ -0,0 +1,151 @@
+import s from './checkoutForm.module.css'
+import { Button } from "@material-ui/core";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+
+// TODO: Fetch from store
+const customerEmail = "test3@hejtest.com";
+const customerFullName = "Mrs Test"
+const priceIdFromStore = 'price_1IRdujA3UXZjjLWxbwLujK5l';
+
+export default function CheckoutForm() {
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [customerId, setCustomerId] = useState('');
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  useEffect(() => {
+    // Create a Stripe customer as soon as the page loads
+    window
+      .fetch("https://localhost:5001/api/payments/customers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email: customerEmail, fullName: customerFullName})
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setCustomerId(data?.id);
+      });
+  }, []);
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d"
+        }
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a"
+      }
+    }
+  };
+
+  const handleChange = async (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  // Create payment method towards Stripe
+  function createPaymentMethod() {
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+
+    let billingName = customerFullName;
+    let priceId = priceIdFromStore;
+
+    stripe
+      .createPaymentMethod({
+        type: 'card',
+        card: cardElement,
+        billing_details: {
+          name: billingName,
+        },
+      })
+      .then((result) => {
+        if (result.error) {
+          setError(result.error);
+        } else {
+          createSubscription({
+            customerId: customerId,
+            paymentMethodId: result.paymentMethod.id,
+            priceId: priceId,
+          });
+        }
+      });
+  };
+
+  // Create subscription
+  function createSubscription({ customerId, paymentMethodId, priceId }) {
+    return (
+      fetch('https://localhost:5001/api/payments/subscriptions', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          customerId: customerId,
+          paymentMethodId: paymentMethodId,
+          priceId: priceId,
+        }),
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        if (result.error) {
+          // If the card is declined, display an error to the user.
+          setError(error);
+          throw result;
+        }
+        setSucceeded(true);
+        return result;
+      })
+      .catch((error) => {
+        setError(error);
+      })
+    );
+  }
+
+  return (
+    <>
+      <CardElement id="card-element" options={cardStyle} onChange={handleChange}/>
+      <Button
+        disabled={processing || disabled || succeeded}
+        id="submit"
+        onClick={createPaymentMethod}
+        >
+        <span id="button-text">
+          {processing ? ("Laddar...") : ("Pay now")}
+        </span>
+      </Button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
+        </div>
+      )}
+      {/* Show a success message upon completion */}
+      {!succeeded && (
+        <div className={s.resultMessage}>
+          Payment succeeded!
+        </div>
+      )}
+    </>
+  );
+}
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { ButtonGroup } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
+import CheckoutForm from "../app/components/CheckoutForm/CheckoutForm";
 
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// loadStripe is initialized with your real test publishable API key.
+const promise = loadStripe("pk_test_51IRGljA3UXZjjLWxcvyxrdMZLfGL3VgavI4xiWcb1TmBWhZ13M8CIxwx9HMLzMtahX6RxxYZcUnJkOAxdfCwIjFE00VCslxaIc");
 
 export default function Checkout( props ) {
   const { t } = useTranslation('checkout');
@@ -19,6 +26,11 @@ export default function Checkout( props ) {
       <br/>
       <span>{t('paymentDetails')}</span><br/>
       {/* Stripe checkout HERE */}
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+      <br/>
+      <br/>
       <span>{t('subtotal')}</span><br/>
       <span>{t('discount')} årsplan</span>
       <br/>
