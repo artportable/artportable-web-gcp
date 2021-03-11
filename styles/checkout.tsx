c487d9146bea297ed3064e6fb62ece092d2f8837
WildWuzzy <jtj.lord@gmail.com>
2021-03-11 10:47:25 +0100

    Add paymentIntervalTabs component and use it in checkout.tsx

---
+++ styles/checkout.tsx
@@ -0,0 +1,9 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+export const checkoutStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      
+    }
+  }),
+);
