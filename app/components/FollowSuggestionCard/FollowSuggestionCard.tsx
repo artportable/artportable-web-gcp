ec803ba4675516ec13f64cdb5db9e3ae3ce14900
Anders <andersroseen@gmail.com>
2021-03-30 16:59:05 +0200

    Break out follow suggestions into its own component

--- app/components/FollowSuggestionCard/FollowSuggestionCard.tsx
+++ app/components/FollowSuggestionCard/FollowSuggestionCard.tsx
@@ -3,18 +3,16 @@ import AvatarSkeleton from '../AvatarSkeleton/AvatarSkeleton'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { CardHeader, Avatar } from '@material-ui/core';
-import { List, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction } from '@material-ui/core';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Button from '../Button/Button'
+import { List } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import { capitalizeFirst } from '../../utils/util';
+import React from 'react';
+import FollowSuggestion from '../FollowSuggestion/FollowSuggestion';
 
 
 export default function FollowSuggestionCard({ suggestedUsers, onFollowClick }) {
   const s = styles();
   const { t } = useTranslation(['feed', 'common']);
 
-  const bucketUrl = 'https://artportable-images.s3.eu-north-1.amazonaws.com/Images/'; // TODO: Fetch from config
   const noOfSuggestions = 5;
 
   return (
@@ -35,32 +33,7 @@ export default function FollowSuggestionCard({ suggestedUsers, onFollowClick }) 
                 </div>
                 ) : (
                 suggestedUsers?.slice(0, noOfSuggestions).map((user) =>
-                  <ListItem key={user.UserId} className={s.listItem}>
-                    <ListItemAvatar>
-                      <Avatar className={s.avatar}>
-                        {user?.ProfilePicture ? (
-                          <Avatar src={`${bucketUrl}${user?.ProfilePicture}`}
-                            alt="Profile picture"
-                            style={{ height: '45px', width: '45px' }}
-                          />
-                        ) : (
-                          <AccountCircleIcon style={{ fontSize: 48 }} color="secondary"></AccountCircleIcon>
-                        )}
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText className={s.listItemText} primary={user.Username} secondary={user.Location} />
-                    <ListItemSecondaryAction className={s.secondaryAction}>
-                      <Button
-                        size="small"
-                        variant="contained"
-                        color="primary"
-                        disableElevation
-                        roundedButton
-                        onClick={() => onFollowClick(user)}>
-                          {capitalizeFirst(t('common:words.follow'))}
-                      </Button>
-                    </ListItemSecondaryAction>
-                  </ListItem>
+                  <FollowSuggestion user={user} onFollowClick={onFollowClick}></FollowSuggestion>
                 )
               )}
           </List>
