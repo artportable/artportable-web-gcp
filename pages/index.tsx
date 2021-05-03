dcb5a164b70f87b44339c5c1f369eb5d060bffce
Anders <andersroseen@gmail.com>
2021-05-03 15:42:17 +0200

    Add Welcome To text section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
+import React, { useState } from 'react'
 import Main from '../app/components/Main/Main'
 import TextCarousel from '../app/components/TextCarousel/TextCarousel'
 import RadioButtonGroup from '../app/components/RadioButtonGroup/RadioButtonGroup'
-import { useState } from 'react'
 import s from '../styles/Home.module.css'
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import Button from '../app/components/Button/Button'
+import Link from 'next/link'
 
 export default function Home( props ) {
+  const { t } = useTranslation(['index', 'header']);
+
   const navItems = props.navItems;
   const [currentTag, setCurrentTag] = useState(navItems[0].tag);
 
@@ -22,6 +26,24 @@ export default function Home( props ) {
           ></RadioButtonGroup>
         </div>
         <h1>Our images &rarr;</h1>
+        <div className={s.welcomeToContainer}>
+          <div className={s.welcomeTo}>
+            <h1>{t('welcomeToTitle')}</h1>
+            <p>{t('welcomeToParagraph')}</p>
+            <Link href="/plans">
+              <a>
+                <Button
+                  size="large"
+                  variant="contained"
+                  color="primary"
+                  disableElevation
+                  rounded>
+                    {t('header:signUp')}
+                </Button>
+              </a>
+            </Link>
+          </div>
+        </div>
       </Main>
     </>
   );
