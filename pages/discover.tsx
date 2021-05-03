b8d4482ab65e54b404627ec706d384c771a866f9
Anders <andersroseen@gmail.com>
2021-05-03 11:11:04 +0200

    Add new Discover page

---
+++ pages/discover.tsx
@@ -0,0 +1,51 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { styles } from '../styles/discover.css';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Main from '../app/components/Main/Main'
+import { useTranslation } from "next-i18next";
+import { Box, Tab, Tabs } from "@material-ui/core";
+import TabPanel from '../app/components/TabPanel/TabPanel'
+import DiscoverArt from "../app/components/DiscoverArt/DiscoverArt";
+import DiscoverArtists from "../app/components/DiscoverArtists/DiscoverArtists";
+
+
+export default function DiscoverPage() {
+  const { t } = useTranslation(['common', 'discover']);
+  const s = styles();
+  const router = useRouter();
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  function a11yProps(index: any) {
+    return {
+      id: `nav-tab-${index}`,
+      'aria-controls': `nav-tabpanel-${index}`,
+    };
+  }
+
+  return (
+    <Main>
+      <Tabs value={activeTab} onChange={(_, newValue) => setActiveTab(newValue)} centered >
+        <Tab label={t('discover:art')} {...a11yProps(t('discover:art'))} />
+        <Tab label={t('discover:artists')} {...a11yProps(t('discover:artists'))} />
+      </Tabs>
+      <Box p={1}>
+        <TabPanel value={activeTab} index={0}>
+          <DiscoverArt></DiscoverArt>
+        </TabPanel>
+        <TabPanel value={activeTab} index={1}>
+          <DiscoverArtists></DiscoverArtists>
+        </TabPanel>
+      </Box>
+    </Main>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return { 
+    props: {
+      ...await serverSideTranslations(locale, ['header', 'common', 'discover']),
+    }
+  };
+}
---
+++ styles/discover.css.tsx
@@ -0,0 +1,10 @@
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles(() =>
+  createStyles({
+    discoverContainer: {
+      backgroundColor: 'var(--background-color)',
+      display: 'grid',
+    },
+  }),
+);
