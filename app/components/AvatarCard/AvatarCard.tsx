bacfe8950fb3964113728451db8a431f36427471
Anders <andersroseen@gmail.com>
2021-05-09 22:23:11 +0200

    Add link to user profile when clicking on avatar card

--- app/components/AvatarCard/AvatarCard.tsx
+++ app/components/AvatarCard/AvatarCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, Link } from "@material-ui/core";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { styles } from "./avatarCard.css";
 import { useTranslation } from "next-i18next";
@@ -13,22 +13,24 @@ export default function AvatarCard({ user }) {
   const bucketUrl = process.env.NEXT_PUBLIC_S3_BUCKET_AWS;
 
   return (
-    <div className={s.container}>
-      {user?.ProfilePicture ? (
-        <Avatar src={`${bucketUrl}${user?.ProfilePicture}`}
-          alt="Profile picture"
-          className={s.avatar}
-        />
-      ) : (
-        <Avatar className={s.avatar}>
-          <AccountCircleIcon style={{ fontSize: 72 }} color="secondary"></AccountCircleIcon>
-        </Avatar>
-      )}
-      <div className={s.text}>
-        <span className={s.username}>{user.Username}</span>
-        <span>{user.Location}</span>
-        <span>{user.Tags.map(tag => capitalizeFirst(t(tag))).join(', ')}</span>
+    <Link href={`/@${user.Username}`} style={{ textDecoration: 'none', color: 'black' }}>
+      <div className={s.container}>
+        {user?.ProfilePicture ? (
+          <Avatar src={`${bucketUrl}${user?.ProfilePicture}`}
+            alt="Profile picture"
+            className={s.avatar}
+          />
+        ) : (
+          <Avatar className={s.avatar}>
+            <AccountCircleIcon style={{ fontSize: 72 }} color="secondary"></AccountCircleIcon>
+          </Avatar>
+        )}
+        <div className={s.text}>
+          <span className={s.username}>{user.Username}</span>
+          <span>{user.Location}</span>
+          <span>{user.Tags.map(tag => capitalizeFirst(t(tag))).join(', ')}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
