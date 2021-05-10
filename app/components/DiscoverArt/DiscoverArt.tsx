377f0279091120ebe0e3813e043ec75e3242bc67
WildWuzzy <jtj.lord@gmail.com>
2021-05-10 17:10:12 +0200

    Add useGetArtworkRows hook

--- app/components/DiscoverArt/DiscoverArt.tsx
+++ app/components/DiscoverArt/DiscoverArt.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { styles } from "./discoverArt.css";
-import { useGetArtworks } from "../../hooks/dataFetching/Artworks";
+import { useGetArtworkRows } from "../../hooks/dataFetching/Artworks";
 import ArtworkListItemDefined from "../ArtworkListItemDefined/ArtworkListItemDefined";
 import { useStore } from 'react-redux';
-import { useMainWidth, useWidth } from '../../hooks/useWidth';
-
-
 
 export default function DiscoverArt({ }) {
   const s = styles();
   const store = useStore();
-  const { data } = useGetArtworks();
-  const mainWidth = useMainWidth().wide;
-
-  const rows = getRows(data, mainWidth);
+  const rows = useGetArtworkRows();
 
   const bucketUrl = process.env.NEXT_PUBLIC_S3_BUCKET_AWS;
   const username = store.getState()?.user?.username;
@@ -52,52 +46,3 @@ export default function DiscoverArt({ }) {
     </Box>
   );
 }
-
-function getRows(artworks, rowWidth) {
-  if (artworks === undefined) { return; }
-
-  const normalized = normalizeHeights(artworks);
-
-  return fillRows(normalized, rowWidth, 100);
-}
-
-function normalizeHeights(artworks) {
-  const defaultHeight = 300;
-
-  return artworks.map(artwork => {
-    const ratio = artwork.PrimaryFile.Width / artwork.PrimaryFile.Height;
-
-    const newWidth = defaultHeight * ratio;
-
-    return { artwork, width: newWidth, height: defaultHeight}
-  });
-}
-
-function fillRows(normalizedArtworks, rowWidth, threshold) {
-  const allowedMinWidth = rowWidth;
-  const slimmed = normalizedArtworks.slice(0, 20);
-  const rows = [];
-
-  let currentRow = [];
-  
-  slimmed.forEach(normalizedArtwork => {
-    const aggregateWidth = currentRow.reduce((accumulated, curr) => accumulated + curr.width, 0);
-    const aggregateSpacings = (currentRow.length - 1) * 16;
-    const totalWidth = aggregateWidth + aggregateSpacings;
-
-
-    if(totalWidth < allowedMinWidth) {
-      currentRow.push(normalizedArtwork);
-    } else {
-      rows.push(currentRow);
-      currentRow = [];
-      currentRow.push(normalizedArtwork);
-    }
-  });
-
-  rows.push(currentRow);
-
-  return rows;
-}
-
-function checkIfArtworkFitsInRow() {}
--- app/hooks/dataFetching/Artworks.tsx
+++ app/hooks/dataFetching/Artworks.tsx
@@ -1,6 +1,15 @@
 import useSWR from 'swr'
 import { Artwork } from '../../models/Artwork';
+import { useMainWidth } from '../useWidth';
+
 const fetcher = url => fetch(url).then(r => r.json().then(data => data))
+
+export function useGetArtworkRows(owner = null) {
+  const { data } = useGetArtworks(owner);
+  const mainWidth = useMainWidth().wide;
+
+  return getRows(data, mainWidth);
+}
 
 export function useGetArtworks(owner = null) {
   const url = new URL("http://localhost:5001/api/artworks");
@@ -71,4 +80,52 @@ export function useGetArtworksForDiscoverPage(tags: Array<string>) {
     isLoading: !error && !data,
     isError: error
   }
+}
+
+
+function getRows(artworks, rowWidth) {
+  if (artworks === undefined) { return; }
+
+  const normalized = normalizeHeights(artworks);
+
+  return fillRows(normalized, rowWidth, 100);
+}
+
+function normalizeHeights(artworks) {
+  const defaultHeight = 300;
+
+  return artworks.map(artwork => {
+    const ratio = artwork.PrimaryFile.Width / artwork.PrimaryFile.Height;
+
+    const newWidth = defaultHeight * ratio;
+
+    return { artwork, width: newWidth, height: defaultHeight}
+  });
+}
+
+function fillRows(normalizedArtworks, rowWidth, threshold) {
+  const allowedMinWidth = rowWidth;
+  const slimmed = normalizedArtworks.slice(0, 20);
+  const rows = [];
+
+  let currentRow = [];
+  
+  slimmed.forEach(normalizedArtwork => {
+    const aggregateWidth = currentRow.reduce((accumulated, curr) => accumulated + curr.width, 0);
+    const aggregateSpacings = (currentRow.length - 1) * 16;
+    const totalWidth = aggregateWidth + aggregateSpacings;
+
+
+    if(totalWidth < allowedMinWidth) {
+      currentRow.push(normalizedArtwork);
+    } else {
+      rows.push(currentRow);
+      currentRow = [];
+      currentRow.push(normalizedArtwork);
+    }
+  });
+
+  rows.push(currentRow);
+
+  return rows;
 }
