8611c1eda7394f9e2b709f3263b4a4a066a4b2ca
Anders <andersroseen@gmail.com>
2021-05-10 09:18:49 +0200

    Remove header in Discover artists

--- app/components/DiscoverArtists/DiscoverArtists.tsx
+++ app/components/DiscoverArtists/DiscoverArtists.tsx
@@ -5,7 +5,6 @@ import { Box } from "@material-ui/core";
 export default function DiscoverArtists({ artists, onFollowClick }) {
   return (
     <Box>
-      <h1>Konstnärer vi tror du hade gillat</h1>
       {artists.map(a =>
         <DiscoverArtistCard artist={a} onFollowClick={onFollowClick}></DiscoverArtistCard>
       )}
