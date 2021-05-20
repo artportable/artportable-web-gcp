33737ad4f8f45544243a23bf1101beb30cd44239
Anders <andersroseen@gmail.com>
2021-05-20 11:29:50 +0200

    Remove PaymentIntervalTabs component

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -7,12 +7,9 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Box from '@material-ui/core/Box';
-import Button from '../app/components/Button/Button';
 import CheckoutForm from "../app/components/CheckoutForm/CheckoutForm";
 import InputLabel from '@material-ui/core/InputLabel';
-
 import { checkoutStyles } from '../styles/checkout';
-import PaymentIntervalTabs, { PaymentInterval } from "../app/components/PaymentIntervalTabs/PaymentIntervalTabs";
 import { useStore } from "../app/redux/store";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
@@ -20,7 +17,7 @@ import { useStore } from "../app/redux/store";
 // loadStripe is initialized with your real test publishable API key.
 const promise = loadStripe("pk_test_51IRGljA3UXZjjLWxcvyxrdMZLfGL3VgavI4xiWcb1TmBWhZ13M8CIxwx9HMLzMtahX6RxxYZcUnJkOAxdfCwIjFE00VCslxaIc");
 
-export default function Checkout( props ) {
+export default function Checkout() {
   const store = useStore();
   const { t } = useTranslation(['checkout', 'common']);
   const styles = checkoutStyles();
@@ -28,8 +25,6 @@ export default function Checkout( props ) {
   const plan = store.getState()?.signup?.price;
   const email = store.getState()?.signup?.data?.email;
   const fullName = store.getState()?.signup?.data?.firstName + ' ' + store.getState()?.signup?.data?.lastName;
-
-  const [selectedPaymentInterval, setSelectedPaymentInterval] = useState<PaymentInterval>('year');
 
   return (
     <Box className={styles.root}>
