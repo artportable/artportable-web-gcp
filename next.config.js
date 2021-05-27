e8bed394055e3d896a88571ec7172e1392989173
Anders <andersroseen@gmail.com>
2021-05-27 16:07:05 +0200

    Change image domain from AWS to Azure

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { i18n } = require('./next-i18next.config')
 module.exports = {
   images: {
-    domains: ['artportable-images.s3.eu-north-1.amazonaws.com'],
+    domains: ['artportabletest.blob.core.windows.net'],
   },
   i18n,
   async rewrites() {
