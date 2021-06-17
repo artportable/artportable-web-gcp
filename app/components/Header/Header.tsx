4ea92c6b7bc245ed193725cf1454184700aa19a1
Anders <andersroseen@gmail.com>
2021-06-17 20:49:06 +0200

    Revert "unread chat messages hook"
    This reverts commit 7f2ecac445843a6dfcda7a1a82d4fbc05e83a29e.

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -12,8 +12,9 @@ import { useTranslation } from 'next-i18next'
 import Button from '../Button/Button';
 import I18nSelector from '../I18nSelector/I18nSelector'
 import { styles } from './header.css'
-import React, { useState } from 'react'
-import useSubscribeToUnreadChatMessages from '../../hooks/streamChatUtils/useSubscribeToUnreadChatMessages'
+import React, { useEffect, useState } from 'react'
+import { Avatar } from '@material-ui/core'
+import { useGetChatClient } from '../../hooks/useGetChatClient'
 import ProfileIconButton from '../ProfileIconButton/ProfileIconButton'
 
 export default function Header({ isSignUp, isSignedIn, username = null, profilePicture = null }) {
@@ -23,8 +24,27 @@ export default function Header({ isSignUp, isSignedIn, username = null, profileP
   const containerClasses = `${s.container} ${isSignUp ? s.isSignUp : ''}`;
   const logoHref = isSignedIn ? "/feed" : "/";
   const [unreadChatMessages, setUnreadChatMessages] = useState(0);
+  const [chatClient] = useState(useGetChatClient(username, profilePicture, isSignedIn, setUnreadChatMessages));
   
-  useSubscribeToUnreadChatMessages(username, profilePicture, isSignedIn, setUnreadChatMessages);
+
+    //TODO: On logout or refresh perhaps, unsubscribe to events to avoid memory leak
+    // https://getstream.io/chat/docs/react/event_listening/?language=javascript#stop-listening-for-events
+
+  useEffect(() => {
+    if(chatClient) {
+      chatClient.on((event) => {
+        if (event.total_unread_count !== undefined) {
+          setUnreadChatMessages(event.total_unread_count);
+        }
+      });
+      
+    }
+
+    return () => {
+      chatClient.off((_) => {});
+    }
+  }, [chatClient]);
+
 
   return (
     <AppBar color="transparent" elevation={0}>
