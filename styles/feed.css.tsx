84f8b53dd4187370149e5d23d5d65671e188c4ca
LordTep <jimmy@boulder.se>
2021-08-18 15:43:36 +0200

    Feed mobile design

--- styles/feed.css.tsx
+++ styles/feed.css.tsx
@@ -8,14 +8,14 @@ export const styles = makeStyles((theme: Theme) =>
       alignItems: 'flex-start',
       gap: '16px',
       gridTemplate: (
-      '\"left feed right\" auto' +
-      '/ minmax(0, 4fr) 8fr minmax(0, 4fr)'),
+      '\"feed\" auto' +
+      '/ 8fr'),
     },
     colLeft: {
+      display: 'none',
       gridArea: 'left',
       position: 'sticky',
       top: 'calc(var(--header-height) + var(--header-box-shadow-padding))',
-      display: 'grid',
       rowGap: theme.spacing(2)
     },
     colFeed: {
@@ -24,15 +24,46 @@ export const styles = makeStyles((theme: Theme) =>
       rowGap: theme.spacing(2),
     },
     colRight: {
+      display: 'none',
       gridArea: 'right',
       position: 'sticky',
       top: 'calc(var(--header-height) + var(--header-box-shadow-padding))',
-      display: 'grid',
       rowGap: theme.spacing(2),
     },
     uploadArtButton: {
       width: '100%',
       height: '3rem'
+    },
+
+    [theme.breakpoints.up('md')]: {
+      feedContainer: {
+        backgroundColor: 'var(--background-color)',
+        display: 'grid',
+        alignItems: 'flex-start',
+        gap: '16px',
+        gridTemplate: (
+        '\"feed right\" auto' +
+        '/ 8fr minmax(0, 4fr)'),
+      },
+      colLeft: {
+        display: 'grid',
+      },
+      colFeed: {
+        gridArea: 'feed',
+        display: 'grid',
+        rowGap: theme.spacing(2),
+      },
+      colRight: {
+        display: 'grid',
+      },
+    },
+
+    [theme.breakpoints.up('lg')]: {
+      feedContainer: {
+        gridTemplate: (
+        '\"left feed right\" auto' +
+        '/ minmax(0, 4fr) 8fr minmax(0, 4fr)'),
+      },
     }
   }),
 );
