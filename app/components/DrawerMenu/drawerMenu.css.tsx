7b28c5adf57009fb395713809238bae6ec3e8cc7
LordTep <jimmy@boulder.se>
2021-08-18 10:30:26 +0200

    Add drawer menu component

---
+++ app/components/DrawerMenu/drawerMenu.css.tsx
@@ -0,0 +1,25 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+
+export const styles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      minWidth: '240px'
+    },
+    listsContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      flexBasis: '100%',
+      justifyContent: 'space-between'
+    },
+    authList: {
+      display: 'initial',
+      paddingBottom: theme.spacing(2)
+    },
+
+    [theme.breakpoints.up('md')]: {
+      authList: {
+        display: 'none',
+      }
+    },
+  }),
+);
