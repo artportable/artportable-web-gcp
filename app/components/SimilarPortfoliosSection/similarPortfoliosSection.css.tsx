af9bcce2325acdaec031f9e3327903b57f3cdf52
LordTep <jimmy@boulder.se>
2021-08-18 17:26:59 +0200

    Similar portfolios section mobile

--- app/components/SimilarPortfoliosSection/similarPortfoliosSection.css.tsx
+++ app/components/SimilarPortfoliosSection/similarPortfoliosSection.css.tsx
@@ -1,13 +1,20 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
-export const styles = makeStyles(() =>
+export const styles = makeStyles((theme: Theme) =>
   createStyles({
     portfolioGrid: {
-      display: 'grid',
-      gap: '15px',
-      gridTemplate: (
-      '\"      first     second     third\"       auto' +
-      '/        1fr        1fr        1fr'),
+      display: 'flex',
+      flexDirection: 'column',
+      gap: theme.spacing(2)
     },
+    [theme.breakpoints.up('mdPlus')]: {
+      portfolioGrid: {
+        display: 'grid',
+        gap: theme.spacing(2),
+        gridTemplate: (
+        '\"      first     second     third\"       auto' +
+        '/        1fr        1fr        1fr'),
+      },
+    }
   }),
 );
