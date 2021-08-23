b972ad8225a368ba8c24fb80b2095914105b1f7b
LordTep <jimmy@boulder.se>
2021-08-23 11:34:57 +0200

    Add tags as chips in discover artist

--- app/components/DiscoverArt/discoverArt.css.tsx
+++ app/components/DiscoverArt/discoverArt.css.tsx
@@ -13,5 +13,25 @@ export const styles = makeStyles((theme: Theme) =>
       justifyContent: 'flex-start',
       overflow: 'hidden'
     },
+    categoryTags: {
+      display: 'flex',
+      gap: theme.spacing(1),
+      paddingInlineStart: 0,
+      listStyleType: 'none',
+      overflow: 'auto',
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
+    },
+    categoryTag: {
+      minWidth: '55px'
+    },
+    selected: {
+      '& .MuiChip-root': {
+        background: theme.palette.primary.dark,
+        border: `1px solid ${theme.palette.primary.dark}`,
+
+      },
+    }
   }),
 );
