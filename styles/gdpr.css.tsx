dbac579fca30a0cbcf823d8c93d2a33a6f630995
LordTep <jtj.lord@gmail.com>
2021-09-24 09:06:57 +0200

    Fast gdpr solution

--- styles/gdpr.css.tsx
+++ styles/gdpr.css.tsx
@@ -4,12 +4,16 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     gdprContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifySelf: 'center',
+      // display: 'flex',
+      // flexDirection: 'column',
+      // alignItems: 'center',
+      // justifySelf: 'center',
       width: '100%',
-      maxWidth: '1080px'
+      maxWidth: '1080px',
+      padding: theme.spacing(10),
+      '& p': {
+        marginBottom: theme.spacing(2)
+      }
     },
     title : {
       textAlign: "center"
