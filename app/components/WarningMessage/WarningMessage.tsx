ef0446f7245e92335e904eb02fda32ad2d01a802
Emil Sundberg <emil.a.sundberg@gmail.com>
2021-09-24 13:51:36 +0200

    information when  upload image

---
+++ app/components/WarningMessage/WarningMessage.tsx
@@ -0,0 +1,19 @@
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { Typography } from '@material-ui/core';
+import { useTranslation } from 'next-i18next';
+
+import styles from './warningMessage.css'
+
+export default function Button(props) {
+  const s = styles();
+  const { t } = useTranslation(['upload']);
+
+  return (
+    <div className={s.information}>
+      <ErrorOutlineIcon className={s.errorOutlineIcon} color="primary" style={{ fontSize: 24 }}></ErrorOutlineIcon>
+      <Typography className={s.informationText}>{t('information')}</Typography>
+    </div>
+  );
+}
+
+
