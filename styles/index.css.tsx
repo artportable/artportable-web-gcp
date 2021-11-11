6257b0c05f8bc85f581fa058de744ee3cbebb7de
Emil Sundberg <emil.a.sundberg@gmail.com>
2021-11-11 10:28:52 +0100

    style for tabs

--- styles/index.css.tsx
+++ styles/index.css.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { rowGap } from '../app/utils/styleUtils';
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,19 +12,29 @@ export const styles = makeStyles((theme: Theme) =>
     },
     tabs: {
       '& .MuiTabs-scroller': {
-        flexGrow: "0"
+        flexGrow: "0",
       },
       justifyContent: "center",
-      minWidth: '54px',
-      width: '100%'
+      width: '100%',
     },
     text: {
-      fontSize: '0.785rem !important',
+      minWidth: 0,
     },
-    [theme.breakpoints.up('sm')]: {
-      text: {
-        fontSize: 'initial !important',
+
+    [theme.breakpoints.up('smPlus')]: {
+        tabs: {
+          '& .MuiTabScrollButton-root': {
+            display: 'none',
+        },
       },
+    },
+
+    [theme.breakpoints.up('md')]: {
+      text: {
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
+      },
+
     },
   }),
 );
