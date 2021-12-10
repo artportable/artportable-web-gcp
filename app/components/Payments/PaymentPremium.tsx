cfa2cc41daa8aaa51f2141fc4a80ef7b645b2296
Emil Sundberg <emil.a.sundberg@gmail.com>
2021-12-10 09:59:08 +0100

    working with css

--- app/components/Payments/PaymentPremium.tsx
+++ app/components/Payments/PaymentPremium.tsx
@@ -1,20 +1,98 @@
-import { Paper } from '@material-ui/core'
+import { useState } from 'react';
+import { Paper, TextField, FormControl, Input, FormHelperText, OutlinedInput, InputLabel, Typography, Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'next-i18next' 
 import { styles } from './paymentPremium.css'
 import Header from '../Header/Header';
+import { SendPaymentInfo, paymentRequest } from './Request';
+
+const useStyles = makeStyles((theme) => ({
+  accordion: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+    fontSize: '1.5rem'
+  },
+}));
 
 export default function PaymentPremium() {
+  const classes = useStyles();
   const s = styles();
   const { t } = useTranslation(['support']);
+  const [expanded, setExpanded] = useState(false);
+  const [values, setValues] = useState({
+    name: '',
+  });
+  const handleChangesegfeg = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+
   return (
     <>
-    <Header />
-    <Paper elevation={1}>
-      <h1>hej</h1>
-      <img></img>
+    {/* <Header /> */}
+    <Paper elevation={1} className={s.paperDiv}>
+      <div className={s.flexContainer}>
+      <div className={s.left}>
+      <h1>Betala Portfolio Premium</h1>
+      <img width={150}
+        src="/Artportable_Emblem_Gold.svg"
+        alt="hej"
+        title="Premium"/>
       <h2>Premium</h2>
       <h3>12 månader</h3>
-      <p></p>
+      <p>Bli prioriterad i flödet</p>
+      <p> Personlig Konstkoordinator</p>
+      <p>Support</p>
+      <p>Allt från Portfolio</p>
+      </div>
+      <div className={s.right}>
+      <div className={s.input}>
+      <form >
+      <FormControl fullWidth variant="outlined">
+        <OutlinedInput
+          color="secondary"
+          className={s.inputField}
+            id="standard-required-name"
+            value={values.name}
+            onChange={handleChangesegfeg('name')}
+            aria-describedby="standard-name-helper-text"
+            inputProps={{
+              'aria-label': 'Name',
+            }}
+          />
+          <FormHelperText id="standard-name-helper-text" className={s.helperText}>För- och efternamn</FormHelperText>
+        </FormControl>
+       </form>
+       <div className={s.container}>
+      <Accordion className={s.accordion} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>Betalningsalternativ</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </div>
+       </div>
+        
+      </div>
+      </div>
     </Paper>
     </>
   );
--- app/components/Payments/paymentPremium.css.tsx
+++ app/components/Payments/paymentPremium.css.tsx
@@ -2,6 +2,49 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
+    paperDiv: {
+      marginTop: '30px'
+    },
+    flexContainer: {
+      display: 'flex',
+      justifyContent: 'space-around'
+    },
+    right: {
+      backgroundColor: '#EFEEEE',
+      width: '600px',
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    input: {
+      // backgroundColor: 'white',
+      width: '400px',
+      marginTop: '20px',
+    },
+    helperText: {
+      fontSize: '1.2rem',
+      marginLeft: '0',
+    },
+    inputField: {
+      backgroundColor: 'white',
+      width: '400px',
+      fontSize: '1.5rem'
+    },
+    container: {
+      width: '100%',
+    },
+    accordion: {
+      fontSize: '1.5rem',
+      border: `1px solid var(--ion-color-primary)`,
 
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      flexBasis: '33.33%',
+      flexShrink: 0,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+    },
   }),
 );
