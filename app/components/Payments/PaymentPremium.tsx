0393fb6c8b603306e6df0600e5d31a8812dc9377
Emil Sundberg <emil.a.sundberg@gmail.com>
2021-12-13 08:26:46 +0100

    in prgress

--- app/components/Payments/PaymentPremium.tsx
+++ app/components/Payments/PaymentPremium.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { Paper, TextField, FormControl, Input, FormHelperText, OutlinedInput, InputLabel, Typography, Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core'
+import { Paper, TextField, FormControl, Input, FormHelperText, OutlinedInput, InputLabel, Typography, Accordion, AccordionDetails, AccordionSummary, FormLabel, FormControlLabel, RadioGroup, Radio } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'next-i18next' 
 import { styles } from './paymentPremium.css'
 import Header from '../Header/Header';
 import { SendPaymentInfo, paymentRequest } from './Request';
+import Button from '../Button/Button'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 const useStyles = makeStyles((theme) => ({
   accordion: {
@@ -26,7 +28,9 @@ export default function PaymentPremium() {
   const [expanded, setExpanded] = useState(false);
   const [values, setValues] = useState({
     name: '',
+    phoneNumber: '',
   });
+  const [valueRadio, setValueRadio] = useState('');
   const handleChangesegfeg = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
@@ -34,6 +38,10 @@ export default function PaymentPremium() {
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+  const handleChangeRadio = (event) => {
+    setValueRadio(event.target.value);
+  };
+
 
 
   return (
@@ -44,7 +52,7 @@ export default function PaymentPremium() {
       <div className={s.left}>
       <h1>Betala Portfolio Premium</h1>
       <img width={150}
-        src="/Artportable_Emblem_Gold.svg"
+        src="/Images/Artportable_Logotyp_Black.jpg"
         alt="hej"
         title="Premium"/>
       <h2>Premium</h2>
@@ -52,11 +60,15 @@ export default function PaymentPremium() {
       <p>Bli prioriterad i flödet</p>
       <p> Personlig Konstkoordinator</p>
       <p>Support</p>
-      <p>Allt från Portfolio</p>
+      <p>Publicera dina verk</p>
+      <p>Chatta och få förfrågningar om dina konstverk</p>
+      <p>Följ och interagera med andra konstnärer</p>
+      <p>Läs artiklar och få uppdateringar om utställningar</p>
       </div>
       <div className={s.right}>
       <div className={s.input}>
       <form >
+        <Paper>
       <FormControl fullWidth variant="outlined">
         <OutlinedInput
           color="secondary"
@@ -65,12 +77,14 @@ export default function PaymentPremium() {
             value={values.name}
             onChange={handleChangesegfeg('name')}
             aria-describedby="standard-name-helper-text"
+            placeholder="För- och efternamn"
             inputProps={{
               'aria-label': 'Name',
             }}
           />
           <FormHelperText id="standard-name-helper-text" className={s.helperText}>För- och efternamn</FormHelperText>
         </FormControl>
+        </Paper>
        </form>
        <div className={s.container}>
       <Accordion className={s.accordion} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
@@ -82,17 +96,98 @@ export default function PaymentPremium() {
           <Typography className={classes.heading}>Betalningsalternativ</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-            maximus est, id dignissim quam.
-          </Typography>
+        <FormControl component="fieldset">
+            <RadioGroup aria-label="payment" name="payment" value={valueRadio} onChange={handleChangeRadio}>
+              <div className={s.swishFlex}>
+              <FormControlLabel value="swish" control={<Radio />} label={<Typography className={s.radioLabel}>Swish</Typography>} />
+              <img 
+                className={s.swishLogo}
+                width={100}
+                src="/Images/swishlogo.svg"
+                alt="swishlogo"
+                  title="swish"/>
+         </div>
+              {(valueRadio === "swish") && 
+               <div className={s.swishNumber}>
+               <OutlinedInput
+                 color="secondary"
+                 required
+                 className={s.phoneNumber}
+                 id="standard-required-name"
+                 value={values.phoneNumber}
+                 onChange={handleChangesegfeg('phoneNumber')}
+                 aria-describedby="standard-phonenumber-helper-text"
+                 placeholder="Telefonnummer mobil"
+                 inputProps={{
+                   'aria-label': 'phoneNumber',
+                 }}
+               />
+               <FormHelperText id="standard-phonenumber-helper-text" className={s.helperText}>Telefonnummer</FormHelperText>
+               </div>
+              }
+              <div className={s.swishFlex}>
+                <FormControlLabel value="betalkort" control={<Radio />} label={<Typography className={s.radioLabel}>Betalkort</Typography>} />
+                <img 
+                  className={s.s}
+                  width={100}
+                  src="/Images/3_Card_color_horizontal.svg"
+                  alt="swishlogo"
+                    title="swish"/>
+              </div>
+              {(valueRadio === "betalkort") && 
+               <div className={s.swishNumber}>
+               <OutlinedInput
+                 color="secondary"
+                 className={s.phoneNumber}
+                 id="standard-required-name"
+                 value={values.name}
+                 onChange={handleChangesegfeg('name')}
+                 aria-describedby="standard-name-helper-text"
+                 placeholder="Stripebetalning"
+                 inputProps={{
+                   'aria-label': 'Name',
+                 }}
+               />
+                <FormHelperText id="standard-name-card-info-text" className={s.helperText}>Telefonnummer mobil</FormHelperText>
+               </div>
+              }
+            </RadioGroup>
+        </FormControl>
         </AccordionDetails>
       </Accordion>
+      {(valueRadio === "swish" && expanded === 'panel1') && 
+        <Button
+        size="large"
+        fullWidth
+        variant="contained"
+        color="secondary"
+        disableElevation
+        rounded>Betala
+        </Button>
+            }
       </div>
-       </div>
+       </div> 
+      
+       
         
       </div>
-      </div>
+      <div className={s.thirdDiv}>
+        <div className={s.thirdText}>
+      <Typography variant="h4">Öppna Swish på telefonen</Typography>
+          <ArrowDownwardIcon color='secondary' />
+          <Typography variant="h4">Genomför betalningen</Typography>
+          <ArrowDownwardIcon color='secondary'/>
+          <Typography variant="h4">Klart</Typography>
+          </div>
+          <img
+          className={s.swishPhone}
+            width={400}
+            height={450}
+            src="/Images/swishphone.png"
+            alt="swishlogo"
+            title="swish"/>
+        </div>
+        </div>
     </Paper>
     </>
   );
--- app/components/Payments/paymentPremium.css.tsx
+++ app/components/Payments/paymentPremium.css.tsx
@@ -1,40 +1,46 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Height } from '@material-ui/icons';
+import { columnGap, rowGap } from '../../utils/styleUtils'
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     paperDiv: {
-      marginTop: '30px'
+      marginTop: '30px',
+      height: '90vh',
     },
     flexContainer: {
       display: 'flex',
       justifyContent: 'space-around'
     },
     right: {
-      backgroundColor: '#EFEEEE',
+      backgroundColor: '#F5F5F6',
       width: '600px',
       display: 'flex',
       justifyContent: 'center'
     },
     input: {
       // backgroundColor: 'white',
-      width: '400px',
+      width: '500px',
       marginTop: '20px',
+      height: '90vh',
     },
     helperText: {
       fontSize: '1.2rem',
       marginLeft: '0',
+      display: 'none'
     },
     inputField: {
       backgroundColor: 'white',
-      width: '400px',
+      width: '500px',
       fontSize: '1.5rem'
     },
     container: {
       width: '100%',
+      marginTop: '15px'
     },
     accordion: {
       fontSize: '1.5rem',
-      border: `1px solid var(--ion-color-primary)`,
+      border: `1px solid #cfcfcf`,
 
     },
     heading: {
@@ -45,6 +51,53 @@ export const styles = makeStyles((theme: Theme) =>
     secondaryHeading: {
       fontSize: theme.typography.pxToRem(15),
       color: theme.palette.text.secondary,
+    },
+    radioLabel: {
+        fontSize: '1.2rem',
+    },
+    swishFlex: {
+      display: 'flex'
+
+    },
+    swishNumber: {
+      marginBottom: '15px',
+      transition: 'width 10s, height 10s',
+    },
+    swishLogo: {
+      marginLeft: '25px',
+      position: 'relative',
+      top: '5px'
+    },
+    phoneNumber: {
+      backgroundColor: 'white',
+      width: '300px',
+      fontSize: '1.2rem',
+      transition: 'width 10s, height 10s',
+    },
+    stripeFlex: {
+      
+    },
+    s: {
+      marginTop: '5px'
+    },
+    thirdDiv: {
+      display: 'flex',
+      width: '900px',
+      backgroundColor: '#F5F5F6',
+      borderRadius: '800px 0px 0px 800px',
+      justifyContent: 'center',
+
+    },
+    thirdText: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      ...columnGap(theme.spacing(2)),
+      
+    },
+    swishPhone: {
+      alignSelf: 'center',
     },
   }),
 );
