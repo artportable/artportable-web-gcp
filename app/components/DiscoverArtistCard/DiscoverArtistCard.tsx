c5b14ddd501d641ba74ee4992f95966b609654a1
Emil Sundberg <emil.a.sundberg@gmail.com>
2022-02-23 10:53:31 +0100

    use mui link instead of next

--- app/components/DiscoverArtistCard/DiscoverArtistCard.tsx
+++ app/components/DiscoverArtistCard/DiscoverArtistCard.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Link from 'next/link'
 import { styles } from "./discoverArtistCard.css";
 import { useTranslation } from "next-i18next";
 import Button from "../Button/Button";
@@ -8,7 +7,7 @@ import AvatarCard from "../AvatarCard/AvatarCard";
 import Paper from "@material-ui/core/Paper";
 import clsx from 'clsx'
 import { useRef } from "react";
-import { IconButton, Theme, useTheme } from "@material-ui/core";
+import { IconButton, Theme, useTheme, Link } from "@material-ui/core";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { normalizeImageSize } from "../../utils/layoutUtils";
@@ -49,45 +48,43 @@ export default function DiscoverArtistCard({ artist, onFollowClick }) {
             size="small"
             variant={!isFollowed ? "contained" : "outlined"}
             color="primary"
-            startIcon={!isFollowed ? <AddIcon/> : null}
+            startIcon={!isFollowed ? <AddIcon /> : null}
             disableElevation
             rounded
             className={s.button}
-            onClick={() => { toggleFollow(); trackGoogleAnalytics(ActionType.FOLLOW_DISCOVER, CategoryType.INTERACTIVE);}}>
-              {capitalizeFirst(
-                !isFollowed ?
-                  t('common:words.follow') :
-                  t('common:words.following')
-              )}
+            onClick={() => { toggleFollow(); trackGoogleAnalytics(ActionType.FOLLOW_DISCOVER, CategoryType.INTERACTIVE); }}>
+            {capitalizeFirst(
+              !isFollowed ?
+                t('common:words.follow') :
+                t('common:words.following')
+            )}
           </Button>
         }
       </div>
       <div className={s.scrollContainer}>
         <div ref={scrollRef} className={clsx(s.row, s.scroll, s.rowFlex)}>
-          {images.map((image, i) => 
+          {images.map((image, i) =>
             <div className={clsx(s.rowFlex)} key={i}>
               <Paper key={image.Name} className={s.imagePaper} variant="outlined">
                 <Link href={`/art/${image.id}`}>
-                  <a>
-                    <img src={`${bucketUrl}${image.Name}`}
-                      alt={image.title}
-                      width={image.Width}
-                      height={image.Height}
-                    />
-                  </a>
+                  <img src={`${bucketUrl}${image.Name}`}
+                    alt={image.title}
+                    width={image.Width}
+                    height={image.Height}
+                  />
                 </Link>
               </Paper>
             </div>
           )}
         </div>
         <IconButton className={s.leftButton} color="primary" onClick={() => {
-            scrollRef.current.scrollBy({ top: 0, left: -scrollBy, behavior: 'smooth'});
-          }}>
-            <ChevronLeftIcon className={s.chevron}></ChevronLeftIcon>
+          scrollRef.current.scrollBy({ top: 0, left: -scrollBy, behavior: 'smooth' });
+        }}>
+          <ChevronLeftIcon className={s.chevron}></ChevronLeftIcon>
         </IconButton>
         <IconButton className={s.rightButton} color="primary" onClick={() => {
-            scrollRef.current.scrollBy({ top: 0, left: scrollBy, behavior: 'smooth'});
-          }}>
+          scrollRef.current.scrollBy({ top: 0, left: scrollBy, behavior: 'smooth' });
+        }}>
           <ChevronRightIcon className={s.chevron}></ChevronRightIcon>
         </IconButton>
       </div>
