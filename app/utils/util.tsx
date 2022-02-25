aee7c2f82b20279864edf0e7d020388a437ff1bd
Emil Sveningson <emil@boulder.se>
2022-02-25 02:11:33 +0100

    Add timeout to fetch

--- app/utils/util.tsx
+++ app/utils/util.tsx
@@ -83,3 +83,16 @@ export const getFetcher = (condition, responseValue: ResponseValue = 'json') => 
   }
 
 }
+
+export async function fetchWithTimeout(resource, options) {
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+  return response;
+}
