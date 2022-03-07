ba27fe15e02b65dfa784610ba1ba2f95edd2e4cb
Pontus1991 <70883949+Pontus1991@users.noreply.github.com>
2022-03-07 11:44:08 +0100

    Phone input update

--- app/components/PlanCard/PlanCard.tsx
+++ app/components/PlanCard/PlanCard.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Box, Card, CardContent, Typography, TextField, Tabs } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Tabs,
+} from "@material-ui/core";
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import { useDispatch } from "react-redux";
@@ -9,14 +16,22 @@ import Button from "../Button/Button";
 import PaymentInfo from "../PaymentInfo/PaymentInfo";
 import PlansInfoList from "../PlansInfoList/PlansInfoList";
 import { styles } from "./planCard.css";
-import clsx from 'clsx';
+import clsx from "clsx";
 // import PremiumSignupDialog from '../PremiumSignupDialog/PremiumSignupDialog';
 import PremiumApply from "../PremiumApply/PremiumApply";
 import { PriceData } from "../../../pages/plans";
-import Dialog from '@material-ui/core/Dialog';
-import { ActionType, CategoryType, trackGoogleAnalytics } from "../../utils/googleAnalytics";
+import Dialog from "@material-ui/core/Dialog";
+import {
+  ActionType,
+  CategoryType,
+  trackGoogleAnalytics,
+} from "../../utils/googleAnalytics";
 import { UserContext } from "../../contexts/user-context";
-import { Lead, zapierLeadFreemium, zapierLeadBasic } from "../../utils/zapierLead"
+import {
+  Lead,
+  zapierLeadFreemium,
+  zapierLeadBasic,
+} from "../../utils/zapierLead";
 
 interface Props {
   plan: PriceData;
@@ -25,139 +40,180 @@ interface Props {
   setHideTabs: any;
 }
 
-export default function PlanCard({ plan, hideButtons, lead, setHideTabs }: Props) {
-  const { t } = useTranslation(['plans', 'common', 'checkout']);
+export default function PlanCard({
+  plan,
+  hideButtons,
+  lead,
+  setHideTabs,
+}: Props) {
+  const { t } = useTranslation(["plans", "common", "checkout"]);
   const s = styles();
   const dispatch = useDispatch();
-  const href = plan.product === 'free' ? 'feed' : '/checkout';
+  const href = plan.product === "free" ? "feed" : "/checkout";
   const [isHref, setIsHref] = useState(true);
-  const [isPremiumSignupDialogOpen, setIsPremiumSignupDialogOpen] = useState(false);
-  const planName = t(`plans.${plan.productKey}.name`, `${capitalizeFirst(plan.product)}`);
-  const planSubtitle = t(`plans.${plan.productKey}.subtitle`, `${capitalizeFirst(plan.product)}`);
-  const { email, family_name, given_name, phone, user_type} = useContext(UserContext);
-  const [numberExists, setNumberExists] = useState(true)
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [isTypingNumber, setIsTypingNumber] = useState(false);
-  
-
+  const [isPremiumSignupDialogOpen, setIsPremiumSignupDialogOpen] =
+    useState(false);
+  const planName = t(
+    `plans.${plan.productKey}.name`,
+    `${capitalizeFirst(plan.product)}`
+  );
+  const planSubtitle = t(
+    `plans.${plan.productKey}.subtitle`,
+    `${capitalizeFirst(plan.product)}`
+  );
+  const { email, family_name, given_name, phone, user_type } =
+    useContext(UserContext);
+  const [numberExists, setNumberExists] = useState(true);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   useEffect(() => {
     if (plan.product === "portfolioPremium") {
-      setIsHref(false)
+      setIsHref(false);
     }
   }, [plan]);
 
   function getPriceText() {
-    if (plan.product === 'free') {
-      return '-';
-    } else if (plan.product === 'portfolioPremium' && plan.amount === undefined) {
-      return 'premium';
+    if (plan.product === "free") {
+      return "-";
+    } else if (
+      plan.product === "portfolioPremium" &&
+      plan.amount === undefined
+    ) {
+      return "premium";
     }
 
-    return `${plan.amount} ${plan.currency.toUpperCase()}` +
-      ` / ${t(`common:words.${plan.recurringInterval}`)} (${t('common:words.vat')})`;
-  }
-
-  const handleChange = (e) => {
-    setIsTypingNumber(true);
-    // isTyping.value = isTypingNumber;
-    setPhoneNumber(e.target.value);
-    phone.value = phoneNumber;
+    return (
+      `${plan.amount} ${plan.currency.toUpperCase()}` +
+      ` / ${t(`common:words.${plan.recurringInterval}`)} (${t(
+        "common:words.vat"
+      )})`
+    );
   }
 
   const uppgradeWithPhone = (event) => {
-    if (!phone.value || phone.value === null) {
-      setHideTabs(true)
-      event.stopPropagation();
-      event.preventDefault();
-      setNumberExists(false)
+    setPhoneNumber(event.target.value);
+    phone.value = phoneNumber;
+    setHideTabs(true);
+    event.stopPropagation();
+    event.preventDefault();
+    if (!phone.value) {
       return onNavClick();
     }
-  }
+  };
 
   const onNavClick = () => {
+    setNumberExists(false);
     dispatch({
       type: ADD_PRICE,
-      payload: { ...plan }
+      payload: { ...plan },
     });
-    if (plan.product.toLowerCase() === 'free') {
+    if (plan.product.toLowerCase() === "free") {
       uppgradeWithPhone(event);
       trackGoogleAnalytics(ActionType.SIGN_UP_FREE, CategoryType.BUY);
-      var [userType, interval] = user_type.value.split('-');
-      if (userType === "artist")
-        uppgradeWithPhone(event);
-      return zapierLeadFreemium(lead = {
-        name: { value: given_name.value + ' ' + family_name.value } ?? '',
-        phoneNumber: { value: phone.value } ?? '',
-        email: { value: email.value } ?? '',
-        product: "free",
-        type: { value: user_type.value } ?? ''
-      });
-    } else if (plan.product.toLowerCase() === 'portfolio') {
+      var [userType, interval] = user_type.value.split("-");
+      if (userType === "artist") uppgradeWithPhone(event);
+      return zapierLeadFreemium(
+        (lead = {
+          name: { value: given_name.value + " " + family_name.value } ?? "",
+          phoneNumber: { value: phone.value } ?? "",
+          email: { value: email.value } ?? "",
+          product: "free",
+          type: { value: user_type.value } ?? "",
+        })
+      );
+    } else if (plan.product.toLowerCase() === "portfolio") {
       trackGoogleAnalytics(ActionType.SIGN_UP_PORTFOLIE, CategoryType.BUY);
-      zapierLeadBasic(lead = {
-        name: { value: given_name.value + ' ' + family_name.value } ?? '',
-        phoneNumber: { value: phone.value } ?? '',
-        email: { value: email.value } ?? '',
-        product: "portfolio",
-        type: { value: user_type.value } ?? ''
-      });
+      zapierLeadBasic(
+        (lead = {
+          name: { value: given_name.value + " " + family_name.value } ?? "",
+          phoneNumber: { value: phone.value } ?? "",
+          email: { value: email.value } ?? "",
+          product: "portfolio",
+          type: { value: user_type.value } ?? "",
+        })
+      );
     }
     return true;
-  }
+  };
 
   return (
     <div className={s.container}>
-      {!numberExists ? 
+      {!numberExists ? (
         <div>
-          <TextField
-            fullWidth
-            placeholder={t('pphone')}
-            value={phoneNumber}
-            required
-            variant="outlined"
-            onChange={handleChange}
-          />
-          <Link passHref href={href}>
-            <a>
-              <Button
-                variant="contained"
-                color="primary"
-                disableElevation
-                rounded
-                onClick={() => setNumberExists(false)}
-               >
-                {t('send')}
-              </Button>
-            </a>
-          </Link>
+          <div className={s.h3}>
+            <h3>
+              För att gå vidare med din uppgradering behöver du ange ditt
+              telefonnummer
+            </h3>
+          </div>
+          <div>
+            <TextField
+              className={s.textField}
+              fullWidth
+              placeholder={t("Telefonnummer...")}
+              value={phoneNumber}
+              required
+              variant="outlined"
+              // onChange={handleChange}
+            />
+            <Link passHref href={href}>
+              <a>
+                <Button
+                  className={s.button}
+                  variant="contained"
+                  color="primary"
+                  disableElevation
+                  rounded
+                  onClick={() => setNumberExists(false)}
+                >
+                  {t("common:words.add")}
+                </Button>
+              </a>
+            </Link>
+          </div>
         </div>
-              : 
-        <Card className={clsx(s.cardRoot, plan.productKey === "portfolio" && s.primaryCard)}>
+      ) : (
+        <Card
+          className={clsx(
+            s.cardRoot,
+            plan.productKey === "portfolio" && s.primaryCard
+          )}
+        >
           <CardContent>
             <Typography variant="h3" component="h2">
-              <Box fontWeight="fontWeightMedium" textAlign="center" fontFamily="LyonDisplay">
+              <Box
+                fontWeight="fontWeightMedium"
+                textAlign="center"
+                fontFamily="LyonDisplay"
+              >
                 {planName}
               </Box>
             </Typography>
 
             <Typography variant="subtitle1" component="h3">
-              <Box fontWeight="fontWeightMedium" textAlign="center" fontFamily="LyonDisplay">
+              <Box
+                fontWeight="fontWeightMedium"
+                textAlign="center"
+                fontFamily="LyonDisplay"
+              >
                 {planSubtitle}
               </Box>
             </Typography>
 
             <PaymentInfo
               priceText={getPriceText()}
-              secondaryText={t('youCanAlwaysUpdateYourMembership')}
+              secondaryText={t("youCanAlwaysUpdateYourMembership")}
             />
-            <PlansInfoList texts={t(`plans.${plan.productKey}.listTexts`, '', { returnObjects: true })} />
-
+            <PlansInfoList
+              texts={t(`plans.${plan.productKey}.listTexts`, "", {
+                returnObjects: true,
+              })}
+            />
 
             {/* // "VÄLJ PORTFOLIO-KNAPP" */}
-            {!hideButtons &&
+            {!hideButtons && (
               <div className={s.button}>
-                {isHref ?
+                {isHref ? (
                   <Link passHref href={href}>
                     <a>
                       <Button
@@ -168,41 +224,49 @@ export default function PlanCard({ plan, hideButtons, lead, setHideTabs }: Props
                         rounded
                         onClick={(event) => uppgradeWithPhone(event)}
                       >
-                        {plan.product === 'free' ?
-                          t('signUp') :
-                          `${capitalizeFirst(t('common:words.choose'))} ${planName}`
-                        }
+                        {plan.product === "free"
+                          ? t("signUp")
+                          : `${capitalizeFirst(
+                              t("common:words.choose")
+                            )} ${planName}`}
                       </Button>
                     </a>
                   </Link>
-                  :
+                ) : (
                   <Button
                     size="small"
                     variant="contained"
                     color="primary"
                     disableElevation
                     rounded
-
-                    onClick={() => { setIsPremiumSignupDialogOpen(true); trackGoogleAnalytics(ActionType.SIGN_UP_PREMIUM, CategoryType.BUY) }}>
-                    {plan.product === 'free' ?
-                      t('signUp') :
-                      `${capitalizeFirst(t('common:words.choose'))} ${planName}`
-                    }
+                    onClick={() => {
+                      setIsPremiumSignupDialogOpen(true);
+                      trackGoogleAnalytics(
+                        ActionType.SIGN_UP_PREMIUM,
+                        CategoryType.BUY
+                      );
+                    }}
+                  >
+                    {plan.product === "free"
+                      ? t("signUp")
+                      : `${capitalizeFirst(
+                          t("common:words.choose")
+                        )} ${planName}`}
                   </Button>
-                }
+                )}
               </div>
-            }
+            )}
           </CardContent>
         </Card>
-      }
+      )}
       <Dialog
         fullWidth
         maxWidth="md"
-        open={isPremiumSignupDialogOpen} onClose={() => setIsPremiumSignupDialogOpen(false)}>
+        open={isPremiumSignupDialogOpen}
+        onClose={() => setIsPremiumSignupDialogOpen(false)}
+      >
         <PremiumApply />
       </Dialog>
-      
     </div>
-  )
+  );
 }
-
