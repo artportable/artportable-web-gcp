8e0cb4d1bd4f1990760863164b4dc13709d68abd
Emil Sundberg <emil.a.sundberg@gmail.com>
2022-03-14 16:46:36 +0100

    new branch and new try for upgrade leads

--- pages/plans.tsx
+++ pages/plans.tsx
@@ -185,8 +185,8 @@ const noPhonenumber = () => {
         <></>
         :
         <div className={s.plansRootContainer}>
-          <div className={s.header}>
-            <Typography variant="h1" align="center">
+          <div className={s.headerDiv}>
+            <Typography className={s.header} variant="h1" align="center">
               <Box fontFamily="LyonDisplay" fontWeight="fontWeightMedium">
                 {t('welcomeTo')}
               </Box>
