5e1117f84fd25c974c744a8589a9a0a343461ca9
L H <linus.hedberg@gmail.com>
2022-03-17 15:19:37 +0100

    Fix maxwidth for artworkpage

--- app/components/IndexHero/IndexHero.tsx
+++ app/components/IndexHero/IndexHero.tsx
@@ -69,7 +69,7 @@ export default function IndexHero() {
               {t('Hitta originalkonst')}
             </Typography>
             <Typography variant="h4" className={s.description}>
-              {t('Ta en titt i galleriet med över 27500 konstverk')}
+              {t('Ta en titt i galleriet med över 28000 konstverk')}
             </Typography>
               <div className={s.headerButtonArtlover}>
                 <Button
