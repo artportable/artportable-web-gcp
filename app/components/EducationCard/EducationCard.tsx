95c249835bfd7e6665025da6339e5b5b5813d9e3
L H <linus.hedberg@gmail.com>
2022-04-14 12:27:01 +0200

    sort exhibition and education

--- app/components/EducationCard/EducationCard.tsx
+++ app/components/EducationCard/EducationCard.tsx
@@ -17,15 +17,15 @@ export default function EducationCard({ educations }) {
         titleTypographyProps={{ variant: "subtitle1"}}>
       </CardHeader>
       <CardContent>
-        {educations.map((e, i) =>
+        {educations.sort((a, b) => a.From < b.From ? 1 : -1).map((e, i) =>
           <div className={s.container} key={i}>
             <SchoolIcon color="primary" className={s.icon}></SchoolIcon>
             <div className={s.textContainer}>
-              <div className={clsx(s.yearsRow, (e.From === 0 || e.To === 0) && s.displayNone)}>
-                <div className={s.years}>{e.From ? e.To ? e.From + " - " + e.To : e.From : e.To ? e.To : ' '}</div>
-              </div>
               <div>
                 {e.Name}
+              </div>
+              <div className={clsx(s.yearsRow, (e.From === 0 || e.To === 0) && s.displayNone)}>
+                <div className={s.years}>{e.From ? e.To ? e.From + " - " + e.To : e.From : e.To ? e.To : ' '}</div>
               </div>
             </div>
           </div>)}
