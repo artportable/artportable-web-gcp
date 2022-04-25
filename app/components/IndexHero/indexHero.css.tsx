4c150f9ac423afcf813abba20edbdb22d37aae91
Emil Sundberg <emil.a.sundberg@gmail.com>
2022-04-25 16:33:03 +0200

    bigger text indexhero

--- app/components/IndexHero/indexHero.css.tsx
+++ app/components/IndexHero/indexHero.css.tsx
@@ -80,8 +80,8 @@ export const styles = makeStyles((theme: Theme) =>
     description: {
       margin: theme.spacing(1, 0,),
       marginBottom: theme.spacing(2),
-      fontSize: '11pt',
-      lineHeight: '1.28',
+      fontSize: '12pt',
+      lineHeight: '1.38',
       [theme.breakpoints.up('sm')]: {
         fontSize: '1.3rem',
       },
