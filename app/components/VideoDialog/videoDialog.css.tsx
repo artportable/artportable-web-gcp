d750162da242a52d649fa3be485f6dc38a148545
Emil Sundberg <emil.a.sundberg@gmail.com>
2022-05-12 16:52:27 +0200

    video dialog in indexhero

---
+++ app/components/VideoDialog/videoDialog.css.tsx
@@ -0,0 +1,30 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles((theme: Theme) =>
+  createStyles({
+    dialog: {
+      '& .MuiDialog-paper': {
+        maxWidth: 'unset',
+        // height: '337.5px'
+      },
+    },
+    videoFrame: {
+      width: '300px',
+      height: '168.75px',
+      [theme.breakpoints.up('sm')]: {
+        width: '400px',
+        height: '225px',
+      },
+      [theme.breakpoints.up('smPlus')]: {
+        width: '500px',
+        height: '281.25px',
+      },
+      [theme.breakpoints.up('md')]: {
+        width: '600px',
+        height: '337.5px',
+      },
+    },
+  }),
+);
+// width: '600px',
+// height: '337.5px',
