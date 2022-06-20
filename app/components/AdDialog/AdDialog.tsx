b5b6e68f6473ccee1644fbf80196169c78ef34e5
Emil Sundberg <emil.a.sundberg@gmail.com>
2022-06-20 09:44:02 +0200

    new ad

--- app/components/AdDialog/AdDialog.tsx
+++ app/components/AdDialog/AdDialog.tsx
@@ -38,9 +38,9 @@ export default function AdDialog(props: Props) {
   const ad = [
     {
       companyName: "Lo Fehrling",
-      companyImageSv: '/ad/loSv.png',
+      companyImageSv: '/ad/lo2Sv.png',
       companyLinkSv: 'https://artportable.com/profile/@lo',
-      companyImageEn: '/ad/loEn.png',
+      companyImageEn: '/ad/lo2En.png',
       companyLinkEn: 'https://artportable.com/en/profile/@lo'
     },
     // {
