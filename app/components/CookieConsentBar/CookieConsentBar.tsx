41a7090c6899f2ee5b27f3183ad076ed77970b6a
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-11-10 09:18:35 +0100

    add new text for cookie banner

--- app/components/CookieConsentBar/CookieConsentBar.tsx
+++ app/components/CookieConsentBar/CookieConsentBar.tsx
@@ -1,5 +1,10 @@
+import { ThemeProvider, Typography } from "@material-ui/core";
 import CookieConsent from "react-cookie-consent";
-import { styles } from './cookieConsentBar.css'
+import { styles } from './cookieConsentBar.css';
+import { theme } from "../../../styles/theme";
+import CookieOutlinedIcon from '@mui/icons-material/CookieOutlined';
+
+
 
 export default function CookieConsentBar() {
     const s = styles();
@@ -7,12 +12,18 @@ export default function CookieConsentBar() {
     return (
         <CookieConsent
             disableStyles={true}
-            buttonText={'OK'}
+            buttonText={'Acceptera'}
             containerClasses={s.cookieConsentBarContainer}
             contentClasses={s.cookieConsentBarContent}
             buttonClasses={s.cookieConsentBarButton}
+            enableDeclineButton flipButtons
+            declineButtonText={'Neka'}
+            declineButtonClasses={s.cookieConsentBarButton}
         >
-            {'This website uses cookies. '}<a href='/gdpr'>Read more</a>
+        <h2><CookieOutlinedIcon />Vi använder cookies på den här webbplatsen.</h2>
+        <h4>Här kan du läsa mer om hur vi använder cookies på vår webbplats. <a href='/gdpr'> Vår cookie policy</a></h4>
         </CookieConsent>
     );
 }
+
+
