b5c911dd6ddf5da868c9c5b268dd462784b32c30
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-01-04 11:13:01 +0100

    format to SEK if null

--- app/components/ArtworkListItemDefined/ArtworkListItemDefined.tsx
+++ app/components/ArtworkListItemDefined/ArtworkListItemDefined.tsx
@@ -45,14 +45,19 @@ export default function ArtworkListItemDefined({
 
   const router = useRouter();
   const excludedCurrencyCodes = ["SEK", "NOK", "DKK"];
-  let formatter: Intl.NumberFormat;
-  if (!excludedCurrencyCodes.includes(artwork.Currency)) {
+
+  let formatter;
+  if (artwork.Currency && !excludedCurrencyCodes.includes(artwork.Currency)) {
     formatter = new Intl.NumberFormat(router.locale, {
       style: "currency",
-      currency: artwork.Currency || "SEK",
+      currency: artwork.Currency,
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     });
+  } else {
+    formatter = {
+      format: (value) => `SEK ${value}`,
+    };
   }
 
   useEffect(() => {
