af910caf71168b5c3e28e25c5f8773295ba0cd90
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-01-04 11:47:40 +0100

    revert formatting

--- app/components/ArtworkListItemDefined/ArtworkListItemDefined.tsx
+++ app/components/ArtworkListItemDefined/ArtworkListItemDefined.tsx
@@ -46,28 +46,22 @@ export default function ArtworkListItemDefined({
   const router = useRouter();
   const excludedCurrencyCodes = ["SEK", "NOK", "DKK"];
 
-  let formatter;
+  let formatter = {
+    format: (value: number) =>
+      `${value.toString().replace(/,/g, "")} ${artwork.Currency || "SEK"}`,
+  };
   if (artwork.Currency && !excludedCurrencyCodes.includes(artwork.Currency)) {
     formatter = new Intl.NumberFormat(router.locale, {
       style: "currency",
       currency: artwork.Currency,
-      minimumFractionDigits: 0, // don't show decimal places
-      maximumFractionDigits: 0, // don't show decimal places
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
     });
   } else {
     formatter = {
       format: (value) => `${value} ${artwork.Currency || "SEK"}`,
     };
   }
-
-  function formattedValue(value) {
-    return {
-      value,
-      formatted: formatter.format(value).replace(/,/g, ""),
-    };
-  }
-
-  const artworkValue = formattedValue(artwork.Price);
 
   useEffect(() => {
     setIsLiked(artwork?.LikedByMe);
@@ -162,7 +156,7 @@ export default function ArtworkListItemDefined({
                 {t("common:words.sold")}{" "}
               </>
             ) : artwork.Price && artwork.Price != "0" ? (
-              artworkValue
+              formatter.format(artwork.Price)
             ) : (
               t("priceOnRequest")
             )}
