a7633320a14e21d75f3b667bd56b1dee7d32aef3
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-01-10 13:42:08 +0100

    fix css cookies

--- app/components/CookieConsentBar/cookieConsentBar.css.tsx
+++ app/components/CookieConsentBar/cookieConsentBar.css.tsx
@@ -6,31 +6,90 @@ export const styles = makeStyles((theme: Theme) =>
       backgroundColor: "var(--background-color)",
       alignItems: "baseline",
       display: "flex",
-      flexWrap: "wrap",
+      flexDirection: "column",
       justifyContent: "space-evenly",
       left: "0",
       bottom: "0",
       position: "fixed",
-      width: "100%",
+      maxWidth: "99%",
       zIndex: 999,
+      padding: "16px 28px",
+      border: "1px solid rgba(0, 0, 0, 0.3)",
+      boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)",
+      margin: "0px 6px 6px",
     },
     cookieConsentBarContent: {
-      margin: "15px",
       "& a": {
-        color: theme.palette.primary.main,
+        color: "#000000",
+        fontWeight: 700,
+        fontSize: "16px",
+        textDecoration: "underline",
       },
     },
     cookieConsentBarButton: {
-      backgroundColor: theme.palette.primary.main,
+      backgroundColor: "#000000",
       color: theme.palette.primary.contrastText,
       border: "0px",
       boxShadow: "none",
       cursor: "pointer",
-      margin: "15px",
-      borderRadius: "24px",
-      padding: "5px 10px",
+      margin: "8px 0px",
+      borderRadius: "5px",
+      padding: "8px 16px",
       paddingLeft: theme.spacing(2),
       paddingRight: theme.spacing(2),
+      width: "100%",
+      height: "56px",
+      fontSize: "16px",
+      fontWeight: 350,
+      textTransform: "uppercase",
+      fontFamily: "Gotham",
+    },
+    cookieConsentBarHeadingClass: {
+      marginTop: 0,
+      marginBottom: "8px",
+    },
+    cookieConsentBarTextClass: {
+      fontSize: "16px",
+      margin: 0,
+    },
+    cookieConsentBarContainerButtons: {
+      width: "100%",
+    },
+
+    [theme.breakpoints.up("smPlus")]: {
+      cookieConsentBarHeadingClass: {
+        fontSize: "24px",
+      },
+      cookieConsentBarContent: {
+        display: "flex",
+      },
+      cookieConsentBarButton: {
+        marginTop: "20px",
+        width: "50%",
+        marginLeft: "60px",
+      },
+      cookieConsentBarContainer: {
+        padding: "40px 37px",
+        minWidth: "99%",
+      },
+      cookieConsentBarTextClass: {
+        marginTop: "20px",
+      },
+      cookieConsentBarIconClass: {
+        marginRight: "16px",
+      },
+    },
+
+    [theme.breakpoints.up("md")]: {
+      cookieConsentBarContainer: {
+        width: "100%",
+      },
+      cookieConsentBarHeadingClass: {
+        fontSize: "32px",
+      },
+      cookieConsentBarButton: {
+        width: "30%",
+      },
     },
   })
 );
