94515c51ed4d0164eef7caeec965d4a548845613
elinaelbrink <e_elbrink@hotmail.com>
2023-01-31 13:58:46 +0100

    cookie modal update

--- app/components/CookieConsentBar/CookieConsentBar.tsx
+++ app/components/CookieConsentBar/CookieConsentBar.tsx
@@ -1,19 +1,35 @@
 import CookieConsent from "react-cookie-consent";
 import { styles } from "./cookieConsentBar.css";
+import { useTranslation } from "next-i18next";
 
 export default function CookieConsentBar() {
   const s = styles();
+  const { t } = useTranslation([ "index" ]);
 
   return (
     <CookieConsent
       disableStyles={true}
-      buttonText={"OK"}
+      buttonText={t("acceptCookies")}
+      buttonWrapperClasses={s.cookieConsentBarContainerButtons}
       containerClasses={s.cookieConsentBarContainer}
       contentClasses={s.cookieConsentBarContent}
       buttonClasses={s.cookieConsentBarButton}
     >
-      {"This website uses cookies."}
-      <a href="/gdpr">Read more</a>
+      <div>
+        <img
+          src="Cookies_Icon.svg"
+          className={s.cookieConsentBarIconClass}
+        ></img>
+      </div>
+      <div>
+        <h1 className={s.cookieConsentBarHeadingClass}>
+          {t("weUseCookies")}
+        </h1>
+        <p className={s.cookieConsentBarTextClass}>
+        {t("readMoreCookies")}
+        </p>
+        <a href="/gdpr">{t("cookiePolicy")}</a>
+      </div>
     </CookieConsent>
   );
 }
