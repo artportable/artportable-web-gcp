63de885434d296b0b1ba24e20b7c6de55f27c505
elinaelbrink <e_elbrink@hotmail.com>
2023-02-21 10:07:06 +0100

    small change

--- app/components/AdDialog/AdDialog.tsx
+++ app/components/AdDialog/AdDialog.tsx
@@ -38,13 +38,13 @@ export default function AdDialog(props: Props) {
 
   // if no add, remove the component from index page
   const ad = [
-    /*   {
+    /*{
       companyName: "Plingon",
       companyImageSv: "/ad/plingon.jpg",
       companyLinkSv: "https://bit.ly/3FyutR1",
       companyImageEn: "/ad/plingon.jpg",
       companyLinkEn: "https://bit.ly/3FyutR1",
-    }, */
+    },*/
     {
       companyName: "Artportable",
       companyImageSv: "/ad/sarabergmantakeover.jpg",
