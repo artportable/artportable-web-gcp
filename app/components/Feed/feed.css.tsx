e0771da21f69ad2aca9e5d7286760773bb6d6568
elina <elina@MacBook-Air-som-tillhor-Emil.local>
2023-04-03 11:35:24 +0200

    hide invite button

--- app/components/Feed/feed.css.tsx
+++ app/components/Feed/feed.css.tsx
@@ -3,7 +3,7 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     divInviteButton: {
-      display: 'flex',
+      display: 'none',
       justifyContent: 'center',
       [theme.breakpoints.up('smPlus')]: {
         display: 'none'
