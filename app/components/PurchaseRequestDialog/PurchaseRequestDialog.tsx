e0cbcde2aee952a40b7caed7c6321216e1d26f40
elina <elina@MacBook-Air-som-tillhor-Emil.local>
2023-04-26 15:44:01 +0200

    add purchase request button to feed

--- app/components/PurchaseRequestDialog/PurchaseRequestDialog.tsx
+++ app/components/PurchaseRequestDialog/PurchaseRequestDialog.tsx
@@ -1,85 +1,104 @@
-import { Checkbox, Dialog, DialogContent, FormGroup, FormControlLabel, TextField, Typography } from '@material-ui/core';
-import { useContext, useEffect, useState } from 'react';
-import Button from '../Button/Button';
+import {
+  Checkbox,
+  Dialog,
+  DialogContent,
+  FormGroup,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { useContext, useEffect, useState } from "react";
+import Button from "../Button/Button";
 import { styles } from "./purchaseRequestDialog.css";
 import { useTranslation } from "next-i18next";
-import { useKeycloak } from '@react-keycloak/ssr';
-import type { KeycloakInstance } from 'keycloak-js';
+import { useKeycloak } from "@react-keycloak/ssr";
+import type { KeycloakInstance } from "keycloak-js";
 import { useRouter } from "next/router";
-import { ActionType, CategoryType, trackGoogleAnalytics } from '../../utils/googleAnalytics';
+import {
+  ActionType,
+  CategoryType,
+  trackGoogleAnalytics,
+} from "../../utils/googleAnalytics";
 import { UserContext } from "../../contexts/user-context";
 
 export default function PurchaseRequestDialog({ open, onClose, props }) {
-  const { t } = useTranslation(['art', 'common']);
+  const { t } = useTranslation(["art", "common"]);
   const s = styles();
   const { keycloak } = useKeycloak<KeycloakInstance>();
   const router = useRouter();
   const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-  const [messageResponse, setMessageResponse] = useState('');
-  const [customMessage, setCustomMessage] = useState('');
-  const [signUpRedirectHref, setSignUpRedirectHref] = useState('');
-  const [userEmail, setUserEmail] = useState('');
+  const [messageResponse, setMessageResponse] = useState("");
+  const [customMessage, setCustomMessage] = useState("");
+  const [signUpRedirectHref, setSignUpRedirectHref] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
-  const { email} = useContext(UserContext);
+  const { email, isSignedIn } = useContext(UserContext);
 
   useEffect(() => {
     if (email.value) {
-      setUserEmail(email.value)
+      setUserEmail(email.value);
     }
+    console.log("is signed in: " + isSignedIn.value);
+    
   }, []);
 
   const onPurchaseRequestClick = async () => {
     if (userEmail || email.value) {
       // api anrop för att maila/skicka meddelande
-      const response = await fetch(`${apiBaseUrl}/api/messages/purchaserequest?email=${userEmail}&message=${customMessage}&artworkurl=${props.url}&artworkName=${props.title}&artistId=${props.referTo}&artworkImageUrl=${props.imageUrl}`, {
-        method: 'GET',
-      });
-      setMessageResponse(response.status.toString())
+      const response = await fetch(
+        `${apiBaseUrl}/api/messages/purchaserequest?email=${userEmail}&message=${customMessage}&artworkurl=${props.url}&artworkName=${props.title}&artistId=${props.referTo}&artworkImageUrl=${props.imageUrl}`,
+        {
+          method: "GET",
+        }
+      );
+      setMessageResponse(response.status.toString());
     }
-  }
+  };
 
   const onCloseClick = () => {
     onClose();
-    trackGoogleAnalytics(ActionType.PURCHASE_REQUEST_DIALOG_CLOSE, CategoryType.INTERACTIVE)
-    setCustomMessage('');
-    setSignUpRedirectHref('');
-    setMessageResponse('');
-  }
+    trackGoogleAnalytics(
+      ActionType.PURCHASE_REQUEST_DIALOG_CLOSE,
+      CategoryType.INTERACTIVE
+    );
+    setCustomMessage("");
+    setSignUpRedirectHref("");
+    setMessageResponse("");
+  };
 
   useEffect(() => {
     if (props) {
       const isDefaultLocale = router.locale == router.defaultLocale;
-      const artwork = encodeURIComponent(JSON.stringify({
-        title: props.title,
-        creator: props.creator,
-        url: props.url
-      }));
-      const redirectHref = `${window.origin}${isDefaultLocale ? '' : `/${router.locale}`}/${props.pathname}?artwork=${artwork}`
+      const artwork = encodeURIComponent(
+        JSON.stringify({
+          title: props.title,
+          creator: props.creator,
+          url: props.url,
+        })
+      );
+      const redirectHref = `${window.origin}${
+        isDefaultLocale ? "" : `/${router.locale}`
+      }/${props.pathname}?artwork=${artwork}`;
       setSignUpRedirectHref(redirectHref);
     }
   }, []);
 
   return (
     <Dialog open={open} onClose={onCloseClick}>
-      {messageResponse
-        ?
+      {messageResponse ? (
         <DialogContent>
           <Typography variant="h3" className={s.thanksTypo}>
-            {messageResponse == '200' ?
-              t('thanksForInterestTitle')
-              :
-              t('somethingWentWrongTitle')
-            }
+            {messageResponse == "200"
+              ? t("thanksForInterestTitle")
+              : t("somethingWentWrongTitle")}
           </Typography>
 
-          {messageResponse == '200' ?
-            <Typography>
-              {t('thanksForInterestText')}
-            </Typography>
-            :
-            t('somethinWentWrongText')
-          }
+          {messageResponse == "200" ? (
+            <Typography>{t("thanksForInterestText")}</Typography>
+          ) : (
+            t("somethinWentWrongText")
+          )}
           <div className={s.buttonContainer}>
             <Button
               variant="outlined"
@@ -88,59 +107,63 @@ export default function PurchaseRequestDialog({ open, onClose, props }) {
               rounded
               onClick={onCloseClick}
             >
-              {t('common:words.close')}
+              {t("common:words.close")}
             </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              disableElevation
-              rounded
-              onClick={() => {
-                keycloak.register({
-                  locale: router.locale,
-                  redirectUri: signUpRedirectHref
-                });
-                trackGoogleAnalytics(ActionType.SIGN_UP_PURCHASE_REQUEST_AFTER, CategoryType.BUY);
-              }}
-            >
-              {t('createAccountToChat')}
-            </Button>
+         {!isSignedIn.value && (
+             <Button
+             variant="contained"
+             color="primary"
+             disableElevation
+             rounded
+             onClick={() => {
+               keycloak.register({
+                 locale: router.locale,
+                 redirectUri: signUpRedirectHref,
+               });
+               trackGoogleAnalytics(
+                 ActionType.SIGN_UP_PURCHASE_REQUEST_AFTER,
+                 CategoryType.BUY
+               );
+             }}
+           >
+             {t("createAccountToChat")}
+           </Button>
+
+         )
+
+         }
           </div>
         </DialogContent>
-        :
+      ) : (
         <DialogContent>
           <form className={s.form}>
             <Typography variant="h5" component="h2" className={s.sendMailTypo}>
-              {t('sendEmailToArtist')}
+              {t("sendEmailToArtist")}
             </Typography>
-            <Typography>
-            {t('fillInYourEmail')}
-            </Typography>
+            <Typography>{t("fillInYourEmail")}</Typography>
             <TextField
               classes={{
-                root: s.textField
+                root: s.textField,
               }}
               fullWidth
               id="email"
-              placeholder={t('yourEmail')}
+              placeholder={t("yourEmail")}
               value={email.value ? email.value : null}
               onChange={(e) => setUserEmail(e.target.value)}
               variant="outlined"
-            >
-            </TextField>
+            ></TextField>
             <TextField
               classes={{
-                root: s.textFieldMultiline
+                root: s.textFieldMultiline,
               }}
               fullWidth
               multiline
               rows={5}
               inputProps={{ maxLength: 500 }}
-              placeholder={t('common:messageOptional')}
+              placeholder={t("common:messageOptional")}
               onChange={(e) => setCustomMessage(e.target.value)}
               variant="outlined"
-            >
-            </TextField>
+            ></TextField>
             <FormGroup>
               <Button
                 variant="contained"
@@ -150,15 +173,18 @@ export default function PurchaseRequestDialog({ open, onClose, props }) {
                 className={s.messageButton}
                 onClick={() => {
                   onPurchaseRequestClick();
-                  trackGoogleAnalytics(ActionType.PURCHASE_REQUEST_SEND_SIGNED_OUT, CategoryType.BUY)
+                  trackGoogleAnalytics(
+                    ActionType.PURCHASE_REQUEST_SEND_SIGNED_OUT,
+                    CategoryType.BUY
+                  );
                 }}
               >
-                {t('SendPurchaseRequest')}
+                {t("SendPurchaseRequest")}
               </Button>
             </FormGroup>
           </form>
         </DialogContent>
-      }
+      )}
     </Dialog>
   );
 }
