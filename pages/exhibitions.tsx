0d17701bb5d26b36f4c1769e50e3f765fc30c124
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-06-11 01:04:06 +0200

    empty exhibition page

--- pages/exhibitions.tsx
+++ pages/exhibitions.tsx
@@ -1,32 +1,15 @@
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
-import Main from "../app/components/Main/Main";
-import { getNavBarItems } from "../app/utils/getNavBarItems";
-
-export default function Exhibition({ navBarItems }) {
+export default function Exhibitions() {
   return (
     <>
-      <Main navBarItems={navBarItems}>
-        <Head>
-          <title>Lediga jobbannonser</title>
-        </Head>
-        <div>Utställningar</div>
-      </Main>
+      <title>Lediga jobbannonser</title>
+      <div>Utställningar</div>
     </>
   );
 }
 
 export async function getStaticProps({ locale }) {
-  const navBarItems = await getNavBarItems();
   return {
-    props: {
-      navBarItems: navBarItems,
-      ...(await serverSideTranslations(locale, [
-        "header",
-        "footer",
-        "support",
-      ])),
-    },
+    props: {},
     revalidate: 60,
   };
 }
