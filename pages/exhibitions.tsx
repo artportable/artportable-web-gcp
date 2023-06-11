94fdf844d35b6bab993bcf9ad41decc5dfa40bb2
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-06-11 10:04:46 +0200

    bug fix

--- pages/exhibitions.tsx
+++ pages/exhibitions.tsx
@@ -1,15 +1,7 @@
 export default function Exhibitions() {
   return (
     <>
-      <title>Lediga jobbannonser</title>
       <div>Utställningar</div>
     </>
   );
-}
-
-export async function getStaticProps({ locale }) {
-  return {
-    props: {},
-    revalidate: 60,
-  };
 }
