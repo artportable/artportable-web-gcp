976ee0eeb4c423c84d2f5cb18cdfc41dfa351776
MassiWR <90767729+MassiWR@users.noreply.github.com>
2023-06-11 11:22:33 +0200

    add translations

--- pages/exhibitions.tsx
+++ pages/exhibitions.tsx
@@ -1,9 +1,15 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { getNavBarItems } from "../app/utils/getNavBarItems";
+import { Head } from "next/document";
+import { useTranslation } from "next-i18next";
 
 export default function Exhibitions() {
+  const { t } = useTranslation("exhibitions");
   return (
     <>
+      <Head>
+        <title>{t("title")}</title>
+      </Head>
       <div>Utställningar</div>
     </>
   );
