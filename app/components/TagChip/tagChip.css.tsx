a91dc7323ad46fcbaa3c4c00dbad39323290f8df
MassiWR <massi.wararoodi@chasacademy.se>
2023-09-19 15:47:44 +0200

    style tagchip

--- app/components/TagChip/tagChip.css.tsx
+++ app/components/TagChip/tagChip.css.tsx
@@ -1,21 +1,412 @@
+import { red } from "@material-ui/core/colors";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
-    chip: {
-      marginRight: "5px",
-      marginBottom: "5px",
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        smPlus: 820,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
     },
-    smallTag: {
-      backgroundColor: "transparent",
-      border: "1px solid #3e3e3e",
-      margin: "1px",
+    container: {
+      display: "inline-block",
+      "&:hover $title": {
+        visibility: "hidden",
+        opacity: 1,
+      },
+      "&:hover $tagsContainer": {
+        visibility: "visible",
+        opacity: 1,
+      },
+
+      "&:hover $newUser": {
+        visibility: "hidden",
+        opacity: 0,
+      },
+      [theme.breakpoints.up("md")]: {
+        "&:hover $chatButton": {
+          visibility: "visible",
+          opacity: 1,
+        },
+        "&:hover $shareButton": {
+          visibility: "visible",
+          opacity: 1,
+        },
+      },
+    },
+    imageContainer: {
+      borderRadius: "2px",
+      position: "relative",
+      overflow: "hidden",
+      textAlign: "center",
+      "& > div:not( $newUserWrapper)": {
+        visibility: "hidden",
+        opacity: 0,
+        transition: "opacity 0.3s linear",
+      },
+
+      "& >  $newUserWrapper  $newUser": {
+        visibility: "visible",
+        opacity: 1,
+      },
+
+      "&:hover > div:not( $newUserWrapper)": {
+        visibility: "visible",
+        opacity: 1,
+      },
+      "&:hover $newUser": {
+        visibility: "hidden",
+        opacity: 0,
+      },
+    },
+
+    editOverlay: {
+      position: "absolute",
+      top: 0,
+      visibility: "hidden",
+      "@media (hover: none)": {
+        visibility: "visible",
+      },
+      width: "100%",
+      boxShadow: `${theme.palette.common.black} 0px 0px 40px 25px`,
+    },
+
+    topActions: {
+      position: "absolute",
+      top: 0,
+      right: 0,
+      padding: theme.spacing(1),
+    },
+
+    bottomActionsContainer: {
+      background: "white",
+      visibility: "hidden",
+    },
+
+    bottomActions: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      width: "100%",
+      background: "#fdf9f7", // semi-transparent background
+      borderRadius: "1px",
+      padding: "5px", // adjust as necessary
+      color: "black", // text color, adjust as necessary
+      filter: "drop-shadow(0px 0px 20px rgba(0, 0, 0, 0.4))",
+    },
+    iconInfoContainer: {
+      display: "flex",
+    },
+    titleAndLike: {
+      display: "flex",
+      marginTop: theme.spacing(0.4),
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+      flexGrow: 2,
+      fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.h6.fontSize,
+      // placeItems: 'center'
+    },
+    nameTitleLike: {},
+    likeInline: {
+      display: "inline-block",
+      float: "right",
+      height: "12px",
+    },
+    likeContainer: {
+      display: "flex",
+      marginLeft: "10px",
+      marginBottom: "5px",
+      fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.caption.fontSize,
+      color: theme.palette.text.secondary,
+      placeItems: "center",
+    },
+    newUserWrapper: {},
+    newUser: {
+      position: "absolute",
+      visibility: "visible",
+      opacity: 1,
+      top: 10,
+      right: 10,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       fontSize: "10px",
+      backgroundColor: "rgb(0 0 0 / 57%)",
       borderRadius: "20px",
-      color: "#3e3e3e",
-      height: "20px",
-      width: "auto",
-      filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.2))",
+      fontWeight: "500",
+      color: "white",
+      flexShrink: 0,
+      flexGrow: 0,
+      padding: "10px 15px 10px 15px",
+      boxShadow:
+        "4px 2px 4px rgba(0, 0, 0, 0.5), 4px 2px 8px rgba(0, 0, 0, 0.5)",
+    },
+
+    info: {
+      flexGrow: 2,
+      width: 0,
+      padding: theme.spacing(0.4, 0, 0, 0),
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      flexWrap: "wrap",
+      flexShrink: 1,
+    },
+    name: {
+      fontSize: "0.95rem",
+      fontWeight: 400,
+      textOverflow: "ellipsis",
+      lineClamp: 2,
+    },
+    title: {
+      fontStyle: "italic",
+      fontSize: "0.95rem",
+      height: "1.3rem",
+      marginBottom: "5px",
+
+      [theme.breakpoints.down("sm")]: {
+        visibility: "hidden",
+        opacity: 0,
+      },
+    },
+
+    sizesArt: {
+      fontStyle: "normal",
+      fontSize: "0.75rem",
+      flexShrink: 0,
+      flexGrow: 0,
+    },
+
+    price: {
+      fontSize: "0.8rem",
+      lineHeight: "1rem",
+      fontWeight: theme.typography.fontWeightMedium,
+      display: "flex",
+    },
+
+    soldMark: {
+      background: "#C67777",
+      borderRadius: "50%",
+      width: "15px",
+      height: "15px",
+      marginRight: "5px",
+      display: "flex",
+    },
+    size: {
+      fontSize: "0.8rem",
+      lineHeight: "1rem",
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    likeCounter: {
+      marginLeft: "4px",
+      marginRight: "0",
+
+      padding: theme.spacing(0.9, 0, 0, 0),
+      fontWeight: 500,
+      color: "#000000",
+    },
+    flexLikeCount: {
+      display: "flex",
+    },
+    likeButton: {
+      padding: theme.spacing(0.2, 0, 0.4, 0.5),
+      fontSize: "0.5rem",
+      color: "red !important",
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
+    chatButton: {
+      visibility: "hidden",
+      opacity: 0,
+      transition: "opacity 0.4s linear, visibility 0.9s linear",
+      padding: theme.spacing(0.5, 0, 0.4, 0.5),
+      fontSize: "0.5rem",
+      color: "016A70",
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
+    shareButton: {
+      visibility: "hidden",
+      opacity: 0,
+      transition: "opacity 0.4s linear, visibility 0.9s linear",
+      padding: theme.spacing(0, 0.1, 0.2, 0.5),
+      fontSize: "0.5rem",
+      color: "016A70",
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
+    shareImg: {
+      width: "24px",
+      height: "24px",
+    },
+    purchaseRequestButtonSv: {
+      minWidth: "100px",
+      maxWidth: "100px",
+      maxHeight: "28px",
+      minHeight: "28px",
+      whiteSpace: "nowrap",
+      padding: theme.spacing(0, 2, 0, 2),
+      margin: "1px 1px 1px 1px",
+      border: "1px solid #ffda79",
+      backgroundColor: "#ffda79",
+      color: "#3d3d4e",
+      filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.4))",
+      fontSize: "12px",
+      fontFamily: "gotham",
+      "&:hover": {
+        filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.4))",
+        backgroundColor: "#ffda79",
+        color: "black",
+      },
+    },
+    infoContainer: {
+      maxHeight: "100px",
+      minHeight: "100px",
+    },
+
+    purchaseRequestButtonEn: {
+      minWidth: "100px",
+      maxWidth: "100px",
+      maxHeight: "28px",
+      minHeight: "28px",
+      whiteSpace: "nowrap",
+      padding: theme.spacing(0, 2, 0, 2),
+      margin: "1px 1px 1px 1px",
+      border: "1px solid #ffda79",
+      backgroundColor: "#ffda79",
+      color: "#3d3d4e",
+      filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.4))",
+      fontSize: "12px",
+      fontFamily: "gotham",
+      "&:hover": {
+        filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.4))",
+        backgroundColor: "#ffda79",
+        color: "black",
+      },
+    },
+    inLine: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: "5px",
+      position: "relative",
+    },
+
+    roomButtonSv: {
+      minWidth: "50px",
+      maxWidth: "50px",
+      maxHeight: "28px",
+      minHeight: "28px",
+      whiteSpace: "nowrap",
+      padding: theme.spacing(0, 2, 0, 2),
+      margin: "1px 1px 1px 1px",
+      border: "1px solid #c67777",
+      backgroundColor: "transparent",
+      color: "#FF9191",
+
+      fontSize: "10px",
+
+      fontFamily: "gotham",
+      "&:hover": {
+        border: "1px solid #FF9191",
+        color: "white",
+        backgroundColor: "#FF9191",
+      },
+    },
+    titleTagsContainer: {
+      position: "relative",
+      maxWidth: "100%",
+      flexGrow: 1,
+      flexShrink: 0,
+      textOverflow: "ellipsis",
+      overflow: "hidden",
+    },
+
+    tagsContainer: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      display: "flex",
+      visibility: "hidden",
+      opacity: 0,
+      transition: "opacity 0.3s, visibility 0.3s",
+      flexWrap: "wrap",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      maxWidth: "100%",
+      overFlow: "hidden",
+      [theme.breakpoints.down("sm")]: {
+        visibility: "visible",
+        opacity: 1,
+      },
+    },
+
+    smallTag: {
+      backgroundColor: "#ffda79",
+      border: "1px solid #ffda79",
+      margin: "2px",
+      fontSize: "10px",
+      paddingLeft: "3px",
+      paddingRight: "3px",
+      borderRadius: "20px",
+      color: "#c67777",
+      maxWidth: "100px",
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+      textOverflow: "ellipsis",
+    },
+
+    roomButtonEn: {
+      minWidth: "50px",
+      maxWidth: "50px",
+      maxHeight: "28px",
+      minHeight: "28px",
+      whiteSpace: "nowrap",
+      padding: theme.spacing(0, 2, 0, 2),
+      margin: "1px 1px 1px 1px",
+      border: "1px solid #c67777",
+      backgroundColor: "transparent",
+      color: "#FF9191",
+      fontSize: "10px",
+      fontFamily: "gotham",
+      "&:hover": {
+        border: "1px solid #ffda79",
+        color: "white",
+        backgroundColor: "#FF9191",
+      },
+    },
+
+    deleteGrid: {
+      position: "relative",
+    },
+    deleteButton: {
+      margin: theme.spacing(0.5),
+      padding: 0,
+      position: "absolute",
+      top: theme.spacing(1),
+      right: theme.spacing(1),
+    },
+    purchaseFrameTool: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    rum: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: "5px",
     },
   })
 );
