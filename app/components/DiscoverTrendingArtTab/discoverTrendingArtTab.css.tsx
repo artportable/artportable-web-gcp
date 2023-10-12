4479476bd79bd02b50f6b30ceb82400a74f2cd51
Venessa Karlsson <nessy.right@live.se>
2023-10-12 09:28:24 +0200

    added hover on the tags btn

--- app/components/DiscoverTrendingArtTab/discoverTrendingArtTab.css.tsx
+++ app/components/DiscoverTrendingArtTab/discoverTrendingArtTab.css.tsx
@@ -1,228 +1,222 @@
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     displayTitle: {
       zIndex: 10,
-      color: "#3e3e3e",
+      color: '#3e3e3e',
       fontWeight: 700,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "28px",
-      marginTop: "20px",
-      width: "95%",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "14px",
-        fontWeight: 700,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '28px',
+      marginTop: '20px',
+      width: '95%',
+      [theme.breakpoints.up('xs')]: {
+        fontSize: '14px',
+        fontWeight: 700
       },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "18px",
-        fontWeight: 700,
+      [theme.breakpoints.up('sm')]: {
+        fontSize: '18px',
+        fontWeight: 700
       },
-      [theme.breakpoints.up("smPlus")]: {
-        fontSize: "20px",
-        fontWeight: 700,
+      [theme.breakpoints.up('smPlus')]: {
+        fontSize: '20px',
+        fontWeight: 700
       },
-      [theme.breakpoints.up("md")]: {
-        fontSize: "22px",
-        fontWeight: 700,
+      [theme.breakpoints.up('md')]: {
+        fontSize: '22px',
+        fontWeight: 700
       },
-      [theme.breakpoints.up("mdPlus")]: {
-        fontSize: "24px",
-        fontWeight: 700,
+      [theme.breakpoints.up('mdPlus')]: {
+        fontSize: '24px',
+        fontWeight: 700
       },
-      [theme.breakpoints.up("lg")]: {
-        fontSize: "24px",
-        fontWeight: 700,
-      },
+      [theme.breakpoints.up('lg')]: {
+        fontSize: '24px',
+        fontWeight: 700
+      }
     },
     displayText: {
       zIndex: 10,
-      color: "#3e3e3e",
+      color: '#3e3e3e',
       fontWeight: 400,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      margin: "0 auto",
-      fontSize: "12px",
-      marginTop: "10px",
-      width: "95%",
-      fontStyle: "italic",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "12px",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '0 auto',
+      fontSize: '12px',
+      marginTop: '10px',
+      width: '95%',
+      fontStyle: 'italic',
+      [theme.breakpoints.up('xs')]: {
+        fontSize: '12px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "0px",
+        width: 'auto',
+        position: 'relative',
+        right: '0px'
       },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "14px",
+      [theme.breakpoints.up('sm')]: {
+        fontSize: '14px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "0px",
+        width: 'auto',
+        position: 'relative',
+        right: '0px'
       },
-      [theme.breakpoints.up("smPlus")]: {
-        fontSize: "22px",
+      [theme.breakpoints.up('smPlus')]: {
+        fontSize: '22px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "0px",
+        width: 'auto',
+        position: 'relative',
+        right: '0px'
       },
-      [theme.breakpoints.up("md")]: {
-        fontSize: "22px",
+      [theme.breakpoints.up('md')]: {
+        fontSize: '22px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "35px",
+        width: 'auto',
+        position: 'relative',
+        right: '35px'
       },
-      [theme.breakpoints.up("mdPlus")]: {
-        fontSize: "22px",
+      [theme.breakpoints.up('mdPlus')]: {
+        fontSize: '22px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "35px",
+        width: 'auto',
+        position: 'relative',
+        right: '35px'
       },
-      [theme.breakpoints.up("lg")]: {
-        fontSize: "22px",
+      [theme.breakpoints.up('lg')]: {
+        fontSize: '22px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
-        right: "40px",
-      },
+        width: 'auto',
+        position: 'relative',
+        right: '40px'
+      }
     },
 
     toggleFiltersButton: {
-      display: "block",
-      [theme.breakpoints.up("smPlus")]: {
-        display: "none"
-      },
-      
+      display: 'block',
+      [theme.breakpoints.up('smPlus')]: {
+        display: 'none'
+      }
     },
 
     selectWrapper: {
-      display: "flex",
-        flexDirection: "column",
-        borderRadius: "20px",
-        
-     
-      
+      display: 'flex',
+      flexDirection: 'column',
+      borderRadius: '20px'
     },
 
     selectStyle: {
-      padding: "8px",
-      border: "1px solid black",
-      borderRadius: "20px",
-      margin: "0px 0px 0px 4px",
-      textAlign: "center",
-      cursor: "pointer",
-      fontWeight: "bold",
-      maxWidth: "300px",
-      backgroundColor: "#c67777",
-
+      padding: '8px',
+      border: '1px solid black',
+      borderRadius: '20px',
+      margin: '0px 0px 0px 4px',
+      textAlign: 'center',
+      cursor: 'pointer',
+      fontWeight: 'bold',
+      maxWidth: '300px',
+      backgroundColor: '#c67777'
     },
 
     selectStyleDesktop: {
-      padding: "8px",
-      border: "1px solid black",
-      borderRadius: "20px",
-      margin: "0px 0px 0px 12px",
-      textAlign: "center",
-      cursor: "pointer",
-      fontWeight: "bold",
-      maxWidth: "300px",
-      backgroundColor: "transparent"
+      padding: '8px',
+      border: '1px solid black',
+      borderRadius: '20px',
+      margin: '0px 0px 0px 12px',
+      textAlign: 'center',
+      cursor: 'pointer',
+      fontWeight: 'bold',
+      maxWidth: '300px',
+      backgroundColor: 'transparent'
     },
 
     clearButton: {
-      backgroundColor: "transparent",
-      borderRadius: "20px",
-      border: "1px solid black",
-      cursor: "pointer",
-      padding: "4px 6px 4px 6px",
-      marginLeft: "4px",
-      
+      backgroundColor: 'transparent',
+      borderRadius: '20px',
+      border: '1px solid black',
+      cursor: 'pointer',
+      padding: '4px 6px 4px 6px',
+      marginLeft: '4px'
     },
     selectedTagWrapper: {
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
- 
-      backgroundColor: "transparent"
+
+      backgroundColor: 'transparent'
     },
     selectedTags: {
-      display: "flex",
-      marginTop: "10px",
-      flexDirection: "column-reverse",
-      padding: "10px"
-
+      display: 'flex',
+      marginTop: '10px',
+      flexDirection: 'column-reverse',
+      padding: '10px'
     },
 
     selectedTagsDesktop: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "flex-end",
-      marginBottom: "10px",
-      margin: "4px",
-      border: "1px solid black",
-      borderRadius: "20px",
-      padding: "12px"
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      marginBottom: '10px',
+      margin: '4px',
+      border: '1px solid black',
+      borderRadius: '20px',
+      padding: '12px',
+      cursor: 'pointer',
+      '&:hover': {
+        backgroundColor: 'rgba(0,0, 0, 0.05)'
+      }
     },
     removeTagButton: {
-      marginLeft: "10px",
-      backgroundColor: "transparent",
-      border: "none",
-      cursor: "pointer",
-      fontSize: "14px",
-      fontWeight: "bold"
+      display: 'flex',
+      alignItems: 'center',
+      cursor: 'pointer'
     },
 
     displayTextLatest: {
       zIndex: 10,
-      color: "#3e3e3e",
+      color: '#3e3e3e',
       fontWeight: 400,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      margin: "0 auto",
-      fontSize: "12px",
-      marginTop: "10px",
-      width: "95%",
-      fontStyle: "italic",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "12px",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '0 auto',
+      fontSize: '12px',
+      marginTop: '10px',
+      width: '95%',
+      fontStyle: 'italic',
+      [theme.breakpoints.up('xs')]: {
+        fontSize: '12px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
+        width: 'auto',
+        position: 'relative'
       },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "14px",
+      [theme.breakpoints.up('sm')]: {
+        fontSize: '14px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
+        width: 'auto',
+        position: 'relative'
       },
-      [theme.breakpoints.up("smPlus")]: {
-        fontSize: "16px",
+      [theme.breakpoints.up('smPlus')]: {
+        fontSize: '16px',
         fontWeight: 400,
-        width: "auto",
-        position: "relative",
+        width: 'auto',
+        position: 'relative'
       },
-      [theme.breakpoints.up("md")]: {
-        fontSize: "18px",
+      [theme.breakpoints.up('md')]: {
+        fontSize: '18px',
+        fontWeight: 400
+      },
+      [theme.breakpoints.up('mdPlus')]: {
+        fontSize: '20px',
         fontWeight: 400,
+        width: 'auto'
       },
-      [theme.breakpoints.up("mdPlus")]: {
-        fontSize: "20px",
+      [theme.breakpoints.up('lg')]: {
+        fontSize: '22px',
         fontWeight: 400,
-        width: "auto",
-      },
-      [theme.breakpoints.up("lg")]: {
-        fontSize: "22px",
-        fontWeight: 400,
-        width: "auto",
-      },
-    },
+        width: 'auto'
+      }
+    }
   })
-);
+)
