e37e8d16ef4b147fe9c9e7c3796c922209ac4937
Simon <frasselk@gmail.com>
2023-10-20 08:46:30 +0200

    format

--- app/components/DiscoverArtists/DiscoverArtists.tsx
+++ app/components/DiscoverArtists/DiscoverArtists.tsx
@@ -40,11 +40,11 @@ export default function DiscoverArtists({
   return (
     <Box>
       <Artists />
-      <div style={{ marginBottom: "40px", marginTop: "40px", fontSize: "30px", fontWeight: "bold", display:'flex', flexWrap:'wrap', alignItems:'center' }}>MÅNADENS KONSTNÄR
-      <img
-      src="/Artportable_Emblem_Gold.svg"
-      alt="Logo Artportable"
-      className={s.emblem} />
+      <div style={{ marginBottom: "40px", marginTop: "40px", fontSize: "30px", fontWeight: "bold", display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>MÅNADENS KONSTNÄR
+        <img
+          src="/Artportable_Emblem_Gold.svg"
+          alt="Logo Artportable"
+          className={s.emblem} />
       </div>
       <div className={s.discoverArtistWrapper}>
         {monthlyArtist &&
