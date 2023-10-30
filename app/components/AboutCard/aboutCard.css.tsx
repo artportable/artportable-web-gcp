0d8acfb0d364767fabc737f2637a8b5831f5bdd1
Simon <frasselk@gmail.com>
2023-10-30 08:31:23 +0100

    removed unused css

--- app/components/AboutCard/aboutCard.css.tsx
+++ app/components/AboutCard/aboutCard.css.tsx
@@ -2,31 +2,9 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
-    mainContainer: {
-      display: 'grid',
-      gridTemplateColumns: '1fr', // Single column layout by default
-      gap: '2vw',
-      marginTop: '6vh',
-      [theme.breakpoints.up('md')]: {
-        gridTemplateColumns: '2fr 4fr',
-        gap: '3vw',
-        marginTop: '4vh'
-      }
-    },
-    imgAndTextWrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      [theme.breakpoints.up('md')]: {}
-    },
     imgClass: {
       width: '300px',
       heigh: 'auto'
-    },
-    profilePicture: {
-      display: 'flex',
-      maxHeight: '100%',
-      maxWidth: '100%'
     },
     textContainer: {
       display: 'flex',
@@ -37,7 +15,6 @@ export const styles = makeStyles((theme: Theme) =>
         alignItems: 'flex-start'
       }
     },
-
     profilePicBtn: {
       marginTop: '20px',
       color: 'black',
