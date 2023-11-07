c7e2b671bbf1a0ea903f52b31278fe9db21dbe5b
Massi Wararoodi <massi.wararoodi@outlook.com>
2023-11-07 09:17:32 +0100

    fix education and exhibitions

--- app/components/EditProfileDialog/EditEducation/EditEducation.tsx
+++ app/components/EditProfileDialog/EditEducation/EditEducation.tsx
@@ -1,116 +1,114 @@
-import { TextField, Badge, IconButton } from '@material-ui/core'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
-import DeleteIcon from '@material-ui/icons/Delete'
-import { EditDialogSection } from '../EditDialogSection/EditDialogSection'
-import DateFnsUtils from '@date-io/date-fns'
+import { TextField, Badge, IconButton } from "@material-ui/core";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { EditDialogSection } from "../EditDialogSection/EditDialogSection";
+import DateFnsUtils from "@date-io/date-fns";
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
-} from '@material-ui/pickers'
-import Button from '../../Button/Button'
-import { useTranslation } from 'next-i18next'
-import { styles } from './editEducation.css'
-import { Education } from '../EditProfileDialog'
-import { subYears } from 'date-fns'
-import { v4 } from 'uuid'
+} from "@material-ui/pickers";
+import Button from "../../Button/Button";
+import { useTranslation } from "next-i18next";
+import { styles } from "./editEducation.css";
+import { Education } from "../EditProfileDialog";
+import { subYears } from "date-fns";
+import { v4 } from "uuid";
 
 export const EditEducation = ({ profile, setProfile }) => {
   const s = styles();
-  const { t } = useTranslation('profile');
+  const { t } = useTranslation("profile");
 
   const deleteEducation = (education: Education) => {
-    const newEducations = profile.educations.filter(e => e !== education);
-    setProfile({ ...profile, educations: newEducations })
-  }
-  
+    const newEducations = profile.educations.filter((e) => e !== education);
+    setProfile({ ...profile, educations: newEducations });
+  };
+
   const addEducation = () => {
-    profile.educations.push({ key: v4() });
-    setProfile({...profile});
+    const educations = profile.educations || [];
+    educations.push({ key: v4() });
+    setProfile({ ...profile, educations });
   };
 
   const setFrom = (education: Education, newFrom: Date) => {
     education.from = newFrom?.getFullYear();
     setProfile({ ...profile });
-  }
+  };
 
   const setTo = (education: Education, newTo: Date) => {
     education.to = newTo?.getFullYear();
     setProfile({ ...profile });
-  }
+  };
 
   const setName = (education: Education, newName: string) => {
     education.name = newName;
     setProfile({ ...profile });
-  }
-
-
+  };
 
   return (
-    <EditDialogSection title={t('education')}>
-      {profile?.educations?.map((e, i) => 
-      <Badge
-        key={e.key}
-        overlap="rectangle"
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        classes={{ root: s.badgeRoot }}
-        badgeContent={
-          <IconButton aria-label="delete" onClick={() => deleteEducation(e)}>
-            <DeleteIcon
-              color="error" />
-          </IconButton>
-        }
-      >
-        <div className={s.educationContainer}>
-          <div className={s.fromToContainer}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <EditDialogSection title={t("education")}>
+      {profile?.educations?.map((e, i) => (
+        <Badge
+          key={e.key}
+          overlap="rectangle"
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          classes={{ root: s.badgeRoot }}
+          badgeContent={
+            <IconButton aria-label="delete" onClick={() => deleteEducation(e)}>
+              <DeleteIcon color="error" />
+            </IconButton>
+          }
+        >
+          <div className={s.educationContainer}>
+            <div className={s.fromToContainer}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker
                   autoOk
                   variant="inline"
-                  label={t('from')}
+                  label={t("from")}
                   views={["year"]}
                   minDate={subYears(new Date(), 70)}
                   maxDate={subYears(new Date(), 0)}
-                  value={e.from? new Date(e.from, 0, 1) : null}
-                  InputAdornmentProps={{position: 'start'}}
-                  onChange={date => setFrom(e, date)}
+                  value={e.from ? new Date(e.from, 0, 1) : null}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => setFrom(e, date)}
                 />
               </MuiPickersUtilsProvider>
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker
                   autoOk
                   variant="inline"
-                  label={t('to')}
+                  label={t("to")}
                   views={["year"]}
                   value={e.to ? new Date(e.to, 0, 1) : null}
                   minDate={new Date(e.from, 0, 1)}
                   maxDate={subYears(new Date(), -10)}
-                  InputAdornmentProps={{position: 'start'}}
-                  onChange={date => setTo(e, date)}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => setTo(e, date)}
                 />
               </MuiPickersUtilsProvider>
-          </div>
+            </div>
 
-          <TextField 
-            label={t('education')}
-            defaultValue={e.name}
-            onChange={(event) => setName(e, event.target.value)} 
-            inputProps={{ maxLength: 140 }}
-            helperText={t('maxLength')}
-            />     
-        </div>
-      </Badge>
-      )}
+            <TextField
+              label={t("education")}
+              defaultValue={e.name}
+              onChange={(event) => setName(e, event.target.value)}
+              inputProps={{ maxLength: 140 }}
+              helperText={t("maxLength")}
+            />
+          </div>
+        </Badge>
+      ))}
       <Button
         variant="text"
         color="primary"
         startIcon={<AddCircleOutlineIcon></AddCircleOutlineIcon>}
         onClick={() => addEducation()}
-        >
-          {t('addEducation')}
+      >
+        {t("addEducation")}
       </Button>
     </EditDialogSection>
   );
-}
+};
--- app/components/EditProfileDialog/EditExhibitions/EditExhibitions.tsx
+++ app/components/EditProfileDialog/EditExhibitions/EditExhibitions.tsx
@@ -1,125 +1,128 @@
-import { TextField, Badge, IconButton } from '@material-ui/core'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
-import DeleteIcon from '@material-ui/icons/Delete'
-import { EditDialogSection } from '../EditDialogSection/EditDialogSection'
-import Button from '../../Button/Button'
-import { useTranslation } from 'next-i18next'
-import { styles } from './editExhibitions.css'
+import { TextField, Badge, IconButton } from "@material-ui/core";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { EditDialogSection } from "../EditDialogSection/EditDialogSection";
+import Button from "../../Button/Button";
+import { useTranslation } from "next-i18next";
+import { styles } from "./editExhibitions.css";
 
-import { Exhibition } from '../EditProfileDialog'
-import DateFnsUtils from '@date-io/date-fns'
+import { Exhibition } from "../EditProfileDialog";
+import DateFnsUtils from "@date-io/date-fns";
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
-} from '@material-ui/pickers'
-import { addDays } from 'date-fns'
-import { v4 } from 'uuid'
-
-
+} from "@material-ui/pickers";
+import { addDays } from "date-fns";
+import { v4 } from "uuid";
 
 export const EditExhibitions = ({ profile, setProfile }) => {
   const s = styles();
-  const { t } = useTranslation('profile');
+  const { t } = useTranslation("profile");
 
   const deleteExhibition = (exhibition: Exhibition) => {
-    const newExhibitions = profile.exhibitions.filter(e => e !== exhibition);
-    setProfile({ ...profile, exhibitions: newExhibitions })
-  }
-  
+    const newExhibitions = profile.exhibitions.filter((e) => e !== exhibition);
+    setProfile({ ...profile, exhibitions: newExhibitions });
+  };
+
   const addExhibition = () => {
-    profile.exhibitions.push({ key: v4(), from: new Date(), to: addDays(new Date(), 2)});
-    setProfile({...profile});
+    const exhibitions = profile.exhibitions || [];
+
+    exhibitions.push({
+      key: v4(),
+      from: new Date(),
+      to: addDays(new Date(), 2),
+    });
+    setProfile({ ...profile, exhibitions });
   };
 
   const setFrom = (exhibition: Exhibition, newFrom: Date) => {
     exhibition.from = newFrom;
     setProfile({ ...profile });
-  }
+  };
 
   const setTo = (exhibition: Exhibition, newTo: Date) => {
     exhibition.to = newTo;
     setProfile({ ...profile });
-  }
+  };
 
   const setName = (exhibition: Exhibition, newName: string) => {
     exhibition.name = newName;
     setProfile({ ...profile });
-  }
+  };
 
   const setPlace = (exhibition: Exhibition, newPlace: string) => {
     exhibition.place = newPlace;
     setProfile({ ...profile });
-  }
-
-
+  };
 
   return (
-    <EditDialogSection title={t('exhibitions')}>
-      {profile?.exhibitions?.map((e, i) => 
-      <Badge
-        key={e.key}
-        overlap="rectangle"
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        classes={{ root: s.badgeRoot }}
-        badgeContent={
-          <IconButton aria-label="delete" onClick={() => deleteExhibition(e)}>
-            <DeleteIcon
-              color="error" />
-          </IconButton>
-        }
-      >
-        <div className={s.exhibitionsContainer}>
-          <div className={s.fromToContainer}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDatePicker
-                autoOk
-                variant="inline"
-                label={t('from')}
-                format="dd/MM/yyyy"
-                value={e.from ? e.from : null}
-                InputAdornmentProps={{position: 'start'}}
-                onChange={date => setFrom(e, date)}
-              />
-            </MuiPickersUtilsProvider>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDatePicker
-                autoOk
-                variant="inline"
-                label={t('to')}
-                format="dd/MM/yyyy"
-                value={e.to ? e.to : null}
-                minDate={e.from}
-                InputAdornmentProps={{position: 'start'}}
-                onChange={date => setTo(e, date)}
-              />
-            </MuiPickersUtilsProvider>
-          </div>
+    <EditDialogSection title={t("exhibitions")}>
+      {profile?.exhibitions?.map((e, i) => (
+        <Badge
+          key={e.key}
+          overlap="rectangle"
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          classes={{ root: s.badgeRoot }}
+          badgeContent={
+            <IconButton aria-label="delete" onClick={() => deleteExhibition(e)}>
+              <DeleteIcon color="error" />
+            </IconButton>
+          }
+        >
+          <div className={s.exhibitionsContainer}>
+            <div className={s.fromToContainer}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  autoOk
+                  variant="inline"
+                  label={t("from")}
+                  format="dd/MM/yyyy"
+                  value={e.from ? e.from : null}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => setFrom(e, date)}
+                />
+              </MuiPickersUtilsProvider>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  autoOk
+                  variant="inline"
+                  label={t("to")}
+                  format="dd/MM/yyyy"
+                  value={e.to ? e.to : null}
+                  minDate={e.from}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => setTo(e, date)}
+                />
+              </MuiPickersUtilsProvider>
+            </div>
 
-          <TextField 
-            label={t('name')} 
-            defaultValue={e.name}
-            onChange={(event) => setName(e, event.target.value)}
-            inputProps={{ maxLength: 140 }} />
-          
-          <TextField 
-            label={t('place')} 
-            defaultValue={e.place}
-            onChange={(event) => setPlace(e, event.target.value)}
-            inputProps={{ maxLength: 140 }} />
-        </div>
-      </Badge>
-      )}
+            <TextField
+              label={t("name")}
+              defaultValue={e.name}
+              onChange={(event) => setName(e, event.target.value)}
+              inputProps={{ maxLength: 140 }}
+            />
+
+            <TextField
+              label={t("place")}
+              defaultValue={e.place}
+              onChange={(event) => setPlace(e, event.target.value)}
+              inputProps={{ maxLength: 140 }}
+            />
+          </div>
+        </Badge>
+      ))}
       <Button
         variant="text"
         color="primary"
         startIcon={<AddCircleOutlineIcon></AddCircleOutlineIcon>}
         onClick={() => addExhibition()}
-        >
-          {t('addExhibition')}
+      >
+        {t("addExhibition")}
       </Button>
     </EditDialogSection>
   );
-}
+};
