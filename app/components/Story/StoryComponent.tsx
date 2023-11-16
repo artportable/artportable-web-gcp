f7bac449b8541ff33cbb7f7468d9dc6b5b24bcc4
Simon Lundmark <frasselk@gmail.com>
2023-11-16 10:56:03 +0100

    added storycomponent

--- app/components/Story/StoryComponent.tsx
+++ app/components/Story/StoryComponent.tsx
@@ -1,56 +1,57 @@
 import Link from 'next/link';
-import { styles } from './Story.css';
+import { styles } from './storyComponent.css';
 import { Story } from '../../models/Story';
-
 import { useTranslation } from 'next-i18next';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface StoryComponentProps {
-  story: Story;
+    story: Story;
 }
 
 export default function StoryComponent({ story }: StoryComponentProps) {
-  const { t } = useTranslation('feed');
-  const bucketUrl = process.env.NEXT_PUBLIC_BUCKET_URL;
-  const s = styles();
-  const date: Date = new Date(story?.Published);
-  const month1: string = date.toLocaleString('default', { month: 'short' });
-  const month: string = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
-  const day: string = date.toLocaleString('default', { day: 'numeric' });
+    const { t } = useTranslation('feed');
+    const bucketUrl = process.env.NEXT_PUBLIC_BUCKET_URL;
+    const s = styles();
+    const date: Date = new Date(story?.Published);
+    const month: string = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
+    const day: string = date.toLocaleString('default', { day: 'numeric' });
 
-  return (
-    <article className={s.article}>
-      <Link href={'/'}>
-        <a>
-          <img
-            width={300}
-            alt={`${story?.Title ? story?.Title : 'story image'}`}
-            src={`${bucketUrl}${story?.PrimaryFile?.Name}`}
-          />
-        </a>
-      </Link>
-      <div>
-        <header>
-          <time>
-            <div>
-              <span>{month.toUpperCase()}</span>
-              <span>{day}</span>
-              {/* <span>{day}</span> */}
-            </div>
-          </time>
-          <h2>
-            <Link href={'/'}>
-              <a>
-                <time></time>
-                {story?.Title}
-              </a>
+    useEffect(() => {
+        //console.log(date);
+        console.log(date.toLocaleString());
+        console.log(date.toISOString());
+      })
+
+    return (
+        <article className={s.story}>
+            <Link href={`/story/${story.Id}`}>
+                <a>
+                    <img
+                        style={{ maxWidth: '100%' }}
+                        alt={`${story?.Title ? story?.Title : 'story image'}`}
+                        src={`${bucketUrl}${story?.PrimaryFile?.Name}`}
+                    />
+                </a>
             </Link>
-          </h2>
-        </header>
-        <div>
-          <p>{story?.Description}</p>
-        </div>
-      </div>
-    </article>
-  );
+            <div className={s.textTitle}>
+                <header>
+                    <time dateTime={date.toISOString()} className={s.datePublished}>
+                        <div className={s.monthDay}>
+                            <span className={s.month}>{month}</span>
+                            <span className={s.day}>{day}</span>
+                        </div>
+                    </time>
+                    <h2 className={s.title}>
+                        <Link href={`/story/${story.Id}`}>
+                            <a>
+                                <time></time>
+                                {story?.Title}
+                            </a>
+                        </Link>
+                    </h2>
+                </header>
+                <p className={s.text}>{story?.Description}</p>
+            </div>
+        </article>
+    );
 }
