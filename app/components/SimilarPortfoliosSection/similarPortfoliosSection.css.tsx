4eacc4f53778ba778449381fb59a1ce2873e550b
MassiWR <massi.wararoodi@chasacademy.se>
2023-12-13 11:56:50 +0100

    add margin top similiar portfolios

--- app/components/SimilarPortfoliosSection/similarPortfoliosSection.css.tsx
+++ app/components/SimilarPortfoliosSection/similarPortfoliosSection.css.tsx
@@ -1,20 +1,20 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     portfolioGrid: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: theme.spacing(2)
+      display: "flex",
+      flexDirection: "column",
+      gap: theme.spacing(2),
     },
-    [theme.breakpoints.up('mdPlus')]: {
+    [theme.breakpoints.up("mdPlus")]: {
       portfolioGrid: {
-        display: 'grid',
+        display: "grid",
         gap: theme.spacing(2),
-        gridTemplate: (
-        '\"      first     second     third\"       auto' +
-        '/        1fr        1fr        1fr'),
+        gridTemplate:
+          '"      first     second     third"       auto' +
+          "/        1fr        1fr        1fr",
       },
-    }
-  }),
+    },
+  })
 );
