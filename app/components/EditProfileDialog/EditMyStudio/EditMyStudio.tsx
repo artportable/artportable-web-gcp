ef3b1f8041bd971a70c8f3398fa7b9ae36463c14
Massi <massiwork@MacBook-Air-som-tillhor-Massi.local>
2024-01-03 11:48:02 +0100

    studio edit bug fix

--- app/components/EditProfileDialog/EditMyStudio/EditMyStudio.tsx
+++ app/components/EditProfileDialog/EditMyStudio/EditMyStudio.tsx
@@ -1,75 +1,94 @@
-import { useState } from 'react'
-import Button from '../../Button/Button'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
-import DeleteIcon from '@material-ui/icons/Delete'
+import { useState } from "react";
+import Button from "../../Button/Button";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-
-import { TextField, Typography } from '@material-ui/core'
-import { useTranslation } from 'next-i18next';
-import { styles } from './editMyStudio.css';
+import { TextField, Typography } from "@material-ui/core";
+import { useTranslation } from "next-i18next";
+import { styles } from "./editMyStudio.css";
 
 export const EditMyStudio = ({ profile, setProfile }) => {
   const s = styles();
-  const { t } = useTranslation('profile');
+  const { t } = useTranslation("profile");
 
-  const [hasStudio, setHasStudio] = useState(!!profile.myStudio);
+  const [hasStudio, setHasStudio] = useState(profile?.studio);
 
   const setStudioName = (newName: string) => {
-    setProfile({ ...profile, myStudio: {
-      ...profile.myStudio,
-      Text: newName
-    }})
-  }
+    setProfile({
+      ...profile,
+      studio: {
+        ...profile.studio,
+        Text: newName,
+      },
+    });
+  };
 
   const setStudioLocation = (newLocation: string) => {
-    setProfile({ ...profile, myStudio: {
-      ...profile.myStudio,
-      Location: newLocation
-    }})
-  }
+    setProfile({
+      ...profile,
+      studio: {
+        ...profile.studio,
+        Location: newLocation,
+      },
+    });
+  };
+
+  const removeStudio = () => {
+    setProfile({
+      ...profile,
+      studio: {
+        ...profile.studio,
+        Text: "",
+        Location: "",
+      },
+    });
+    setHasStudio(false);
+  };
 
   return (
     <>
       <div className={s.headerContainer}>
         <Typography variant="subtitle2" component="h4">
-          {t('myArtStudio')}
+          {t("myArtStudio")}
         </Typography>
 
-        {!hasStudio ? 
+        {!profile.studio.Location && !profile.studio.Text ? (
           <Button
             variant="text"
             color="primary"
             startIcon={<AddCircleOutlineIcon></AddCircleOutlineIcon>}
             onClick={() => setHasStudio(true)}
-            >
-              {t('addStudio')}
+          >
+            {t("addStudio")}
           </Button>
-          :
+        ) : (
           <Button
             variant="text"
             startIcon={<DeleteIcon color="error" />}
-            onClick={() => setHasStudio(false)}
-            >
-              {t('remove')}
+            onClick={() => removeStudio()}
+          >
+            {t("remove")}
           </Button>
-        }
+        )}
       </div>
 
-      { hasStudio &&
+      {hasStudio && (
         <div className={s.fieldsContainer}>
-          <TextField 
-            label={t('name')}
-            defaultValue={profile?.myStudio?.Text}
+          <TextField
+            label={t("name")}
+            defaultValue={profile?.studio?.Text}
             onChange={(event) => setStudioName(event.target.value)}
-            inputProps={{ maxLength: 140 }} />
+            inputProps={{ maxLength: 140 }}
+          />
 
-          <TextField 
-            label={t('place')}
-            defaultValue={profile?.myStudio?.Location}
+          <TextField
+            label={t("place")}
+            defaultValue={profile?.studio?.Location}
             onChange={(event) => setStudioLocation(event.target.value)}
-            inputProps={{ maxLength: 140 }} />
+            inputProps={{ maxLength: 140 }}
+          />
         </div>
-      }
+      )}
     </>
   );
-}
+};
