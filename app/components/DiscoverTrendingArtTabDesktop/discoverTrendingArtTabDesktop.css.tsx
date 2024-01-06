d2948273176d775085c1d5e53d9e0fff89fe6b54
Massi <massi.wararoodi@outlook.com>
2024-01-07 00:29:44 +0100

    test

--- app/components/DiscoverTrendingArtTabDesktop/discoverTrendingArtTabDesktop.css.tsx
+++ app/components/DiscoverTrendingArtTabDesktop/discoverTrendingArtTabDesktop.css.tsx
@@ -2,89 +2,86 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
-
     filter: {
-      backgroundColor:'transparent !important',
+      backgroundColor: "transparent !important",
       margin: "10px !important",
     },
 
     filterSummary: {
-      fontSize:'14px',
+      fontSize: "14px",
       fontWeight: 300,
     },
 
     filterDetailsTags: {
       display: "flex",
       flexDirection: "column",
-      backgroundColor:'#fdf9f7', 
-      overflowY: 'scroll',
-      position:'absolute', 
+      backgroundColor: "#fdf9f7",
+      overflowY: "scroll",
+      position: "absolute",
       zIndex: 1,
-      paddingTop: '10px',
-      paddingBottom: '10px',
-      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-      maxHeight:'400px',
-      fontSize:'10px',
+      paddingTop: "10px",
+      paddingBottom: "10px",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+      maxHeight: "400px",
+      fontSize: "10px",
       fontWeight: 500,
-      borderRadius: "10px"
+      borderRadius: "10px",
     },
     filterItemTags: {
-      justifyContent:'start', 
-      width:'auto',
-      paddingBottom:'6px', 
-      paddingTop:'6px', 
-      paddingRight:'16px',
-      paddingLeft:'16px'
+      justifyContent: "start",
+      width: "auto",
+      paddingBottom: "6px",
+      paddingTop: "6px",
+      paddingRight: "16px",
+      paddingLeft: "16px",
     },
 
     filterDetails: {
-      display:'grid', 
-      gridTemplateColumns:'1fr',
-      position: 'absolute', 
-      backgroundColor: '#fdf9f7', 
-      zIndex: 1, 
-      padding: '10px',
-      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-      fontSize:'15px',
-      fontWeight:500,
+      display: "grid",
+      gridTemplateColumns: "1fr",
+      position: "absolute",
+      backgroundColor: "#fdf9f7",
+      zIndex: 1,
+      padding: "10px",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+      fontSize: "15px",
+      fontWeight: 500,
       borderRadius: "10px",
-      
     },
     filterItem: {
-      
-      paddingBottom:'6px', 
-      paddingTop:'6px', 
-      paddingRight:'16px',
-      paddingLeft:'16px',
-      width:'200px',
+      paddingBottom: "6px",
+      paddingTop: "6px",
+      paddingRight: "16px",
+      paddingLeft: "16px",
+      width: "200px",
       fontWeight: "lighter",
-      fontSize: "12px"
+      fontSize: "12px",
     },
     filterIcon: {
-      marginRight:'7px',
+      marginRight: "7px",
     },
     filterClearBtn: {
-      margin: '6px',
+      margin: "6px",
       marginTop: "10px",
-      borderRadius: '20px',
-      height: '30px',
-      width: '100px',
-      textAlign: 'center',
-      border: '1px solid black',
-      color: 'black',
-      fontSize: '12px',
-      padding: '1px',
+      borderRadius: "20px",
+      height: "30px",
+      width: "100px",
+      textAlign: "center",
+      border: "1px solid black",
+      color: "black",
+      fontSize: "12px",
+      padding: "1px",
     },
     selectedTagWrapper: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
 
-      backgroundColor: "transparent"
+      backgroundColor: "transparent",
     },
     selectedTagsDesktop: {
-      textAlign:'center',
+      textAlign: "center",
       display: "flex",
       flexDirection: "row",
       alignItems: "flex-end",
@@ -95,23 +92,25 @@ export const styles = makeStyles((theme: Theme) =>
       padding: "12px",
       cursor: "pointer",
       "&:hover": {
-        backgroundColor: 'rgba(0,0, 0, 0.05)'
-      }
+        backgroundColor: "rgba(0,0, 0, 0.05)",
+      },
     },
     removeTagButton: {
-      display: 'flex',
-      alignItems: 'center',
+      display: "flex",
+      alignItems: "center",
       cursor: "pointer",
     },
     desktopContainer: {
-      display: 'flex',
-      flexDirection: 'row',
-      marginTop: '-40px',
-      marginBottom: '10px',
+      display: "flex",
+      flexDirection: "row",
+      marginTop: "-40px",
+      marginBottom: "10px",
       width: "100%",
-      height: '60px',
-      justifyContent: 'center'
+      height: "60px",
+      justifyContent: "center",
+      [theme.breakpoints.down("smPlus")]: {
+        display: "none",
+      },
     },
-
   })
 );
