2212b1e2170887911c0f88d7d691014f81a5c180
MassiWR <massi.wararoodi@chasacademy.se>
2024-03-06 15:31:20 +0100

    add text for husohem

--- app/hooks/dataFetching/userGetConnectionsCount.tsx
+++ app/hooks/dataFetching/userGetConnectionsCount.tsx
@@ -17,7 +17,7 @@ const fetcher = async (url: string) => {
   const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error("Network response was not ok");
   }
 
   return response.json();
@@ -26,21 +26,19 @@ const fetcher = async (url: string) => {
 export const useGetConnectionsCount = (username: string): FetchResponse => {
   const conditionalFetch = `${apiBaseUrl}/api/user/${username}/connectionscount`;
 
-  const { data, error } = useSWR(
-    conditionalFetch,
-    fetcher,
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
+  const { data, error } = useSWR(conditionalFetch, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
   return {
-    data: data ? {
-      followers: data.Followers,
-      following: data.Followees
-    } : null,
+    data: data
+      ? {
+          followers: data.Followers,
+          following: data.Followees,
+        }
+      : null,
     loading: !error && !data,
-    error: error
+    error: error,
   };
 };
