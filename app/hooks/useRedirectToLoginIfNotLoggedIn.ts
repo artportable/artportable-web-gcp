e5237edc03144cf68edcd4d0b6083d9a5dcab1cf
MassiWR <massi.wararoodi@chasacademy.se>
2024-04-11 16:16:35 +0200

    add a dialog on not logged in like

--- app/hooks/useRedirectToLoginIfNotLoggedIn.ts
+++ app/hooks/useRedirectToLoginIfNotLoggedIn.ts
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import { UserContext } from "../contexts/user-context"
-import { useKeycloak } from '@react-keycloak/ssr'
-import type { KeycloakInstance } from 'keycloak-js'
-import { useRouter } from 'next/router'
+import { UserContext } from "../contexts/user-context";
+import { useKeycloak } from "@react-keycloak/ssr";
+import type { KeycloakInstance } from "keycloak-js";
+import { useRouter } from "next/router";
 import { UrlObject } from "url";
 
 export const useRedirectToLoginIfNotLoggedIn = () => {
@@ -11,14 +11,30 @@ export const useRedirectToLoginIfNotLoggedIn = () => {
   const router = useRouter();
 
   return (originalRedirect?: UrlObject | string) => {
-    if(!isSignedIn.value) {
+    if (!isSignedIn.value) {
       history.pushState(window.location.href, "Artportable");
       keycloak.login({ locale: router.locale });
     } else {
-      if(originalRedirect !== undefined) {
+      if (originalRedirect !== undefined) {
         router.push(originalRedirect);
       }
     }
+  };
+};
 
-  }
-}
+export const useRedirectToRegisterIfNotLoggedIn = () => {
+  const { isSignedIn } = useContext(UserContext);
+  const { keycloak } = useKeycloak<KeycloakInstance>();
+  const router = useRouter();
+
+  return (originalRedirect?: UrlObject | string) => {
+    if (!isSignedIn.value) {
+      history.pushState(window.location.href, "Artportable");
+      keycloak.register({ locale: router.locale });
+    } else {
+      if (originalRedirect !== undefined) {
+        router.push(originalRedirect);
+      }
+    }
+  };
+};
