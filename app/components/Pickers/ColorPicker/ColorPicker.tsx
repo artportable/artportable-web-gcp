e70b10b96b0765c8d75bf5ab4cc5ff65bd485b2b
massiWR <massi.wararoodi@outlook.com>
2024-06-12 16:27:55 +0200

    remove line breaks

--- app/components/Pickers/ColorPicker/ColorPicker.tsx
+++ app/components/Pickers/ColorPicker/ColorPicker.tsx
@@ -1,22 +1,22 @@
 import React, { useContext, useEffect, useState } from "react";
-import clsx from 'clsx';
-import { styles } from './colorpicker.css';
+import clsx from "clsx";
+import { styles } from "./colorpicker.css";
 
 interface Props {
-  COLORS: any[]
-  selectedColor: string
-  setSelectedColor: Function
+  COLORS: any[];
+  selectedColor: string;
+  setSelectedColor: Function;
   // popupText?: string
 }
 // popupText="*Den färg du väljer visas som rubrikbakgrund högst upp på din konstportfolio och ger den en personlig touch.*"
 
 export default function ColorPicker(props: Props) {
   const { COLORS, selectedColor, setSelectedColor } = props;
-  const s = styles({bgColor:'green'});
+  const s = styles({ bgColor: "green" });
 
   return (
     <div className={s.colorPicker}>
-      {COLORS.map(color => {
+      {COLORS.map((color) => {
         const isSelected = color.value === selectedColor;
         const borderWidth = 4;
 
@@ -27,13 +27,13 @@ export default function ColorPicker(props: Props) {
               [s.colorPickerItemSelected]: isSelected,
             })}
             style={{
-              borderColor: isSelected ? 'black' : 'transparent',
+              borderColor: isSelected ? "black" : "transparent",
               backgroundColor: color.value,
             }}
             key={color.value}
           />
-        )
+        );
       })}
     </div>
-  )
+  );
 }
