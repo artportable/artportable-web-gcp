1d482706cad09548fd086cc705089240a07267a3
Lars Fängstam <lf@artworks.io>
2024-06-13 13:32:33 +0200

    Show excerpt from user About on their page if no Headline.

--- app/utils/textUtils.tsx
+++ app/utils/textUtils.tsx
@@ -16,4 +16,27 @@ function insertLinks(text) {
   });
 }
 
-export { renderWithLineBreaks, insertLinks };
+// Find the first period before maxLenght of a string, and return the part before the period.
+// If no period found, return text trucated with ...
+const createExcerpt = (text, maxLength = 0) => {
+  if (!text) return '';
+  if (text && text.length > maxLength) {
+    let excerpt = text.slice(0, maxLength);
+
+    const lastPeriodIndex = excerpt.lastIndexOf('.');
+    if (lastPeriodIndex > 0) {
+      return excerpt.substring(0, lastPeriodIndex + 1);
+    }
+    
+    const lastSpaceIndex = excerpt.lastIndexOf(' ');
+    if (lastSpaceIndex > 0) {
+      excerpt = excerpt.substring(0, lastSpaceIndex);
+    }
+    
+    return excerpt + '...';
+  } else {
+    return text;
+  }
+}
+
+export { renderWithLineBreaks, insertLinks, createExcerpt };
