be56a00899e0df0b857782a0eb182e0db247c789
MassiWR <massi.wararoodi@chasacademy.se>
2024-09-07 22:03:15 +0200

    change toolbar background color

--- app/components/DrawerMenu/drawerMenu.css.tsx
+++ app/components/DrawerMenu/drawerMenu.css.tsx
@@ -1,47 +1,45 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      minWidth: '240px',
-      backgroundColor: 'var(--header-color)',
-      [theme.breakpoints.up('md')]: {
-        minWidth: '300px',
+      minWidth: "240px",
+      backgroundColor: "var(--background-color)",
+      [theme.breakpoints.up("md")]: {
+        minWidth: "300px",
       },
     },
     nested: {
       paddingLeft: theme.spacing(4),
     },
     upgradeButton: {
-      whiteSpace: 'nowrap',
-      width: '130px',
-
-  },
-  closeButtonFlex: {
-    display: 'flex',
-    justifyContent: 'right',
-  },
+      whiteSpace: "nowrap",
+      width: "130px",
+    },
+    closeButtonFlex: {
+      display: "flex",
+      justifyContent: "right",
+    },
     closeButton: {
       top: theme.spacing(1),
-      color: '#000',
-
+      color: "#000",
     },
     spacing: {
-     paddingTop: theme.spacing(8),
-     paddingBottom: theme.spacing(1)
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(1),
     },
     spacingBottom: {
       paddingBottom: theme.spacing(5),
-     },
+    },
     // [theme.breakpoints.up('sm')]: {
     //   languageElement: {
     //     display: 'none',
     //   },
     // },
-    [theme.breakpoints.up('mdPlus')]: {
+    [theme.breakpoints.up("mdPlus")]: {
       articleLink: {
-        display: 'none',
+        display: "none",
       },
     },
-  }),
+  })
 );
