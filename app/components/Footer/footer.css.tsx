997bfde1da85bba8f7642b18238fd2b8978cc55a
massiWR <massi.wararoodi@outlook.com>
2024-09-17 16:24:14 +0200

    fix footer

--- app/components/Footer/footer.css.tsx
+++ app/components/Footer/footer.css.tsx
@@ -1,42 +1,43 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { columnGap, rowGap } from '../../utils/styleUtils'
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { columnGap, rowGap } from "../../utils/styleUtils";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     footer: {
-      display: 'flex',
+      display: "flex",
       padding: theme.spacing(0, 2, 2, 2),
       background: theme.palette.grey[200],
-      backgroundColor: 'var(--footer-color)',
-      boxShadow: '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
-      flexDirection: 'column',
-      flexWrap: 'wrap',
+      backgroundColor: "#ffdd84",
+      boxShadow:
+        "0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)",
+      flexDirection: "column",
+      flexWrap: "wrap",
       // position: 'fixed',
       // left: '0',
       // bottom: '0',
-      width: '100%',
-      [theme.breakpoints.up('smPlus')]: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
+      width: "100%",
+      [theme.breakpoints.up("smPlus")]: {
+        flexDirection: "row",
+        justifyContent: "space-around",
       },
-      [theme.breakpoints.up('md')]: {
-        padding: theme.spacing(0, 8, 2, 8)
+      [theme.breakpoints.up("md")]: {
+        padding: theme.spacing(0, 8, 2, 8),
       },
-      [theme.breakpoints.up('lg')]: {
-        padding: theme.spacing(0, 16, 2, 16)
-      }
+      [theme.breakpoints.up("lg")]: {
+        padding: theme.spacing(0, 16, 2, 16),
+      },
     },
     flexItem: {
-      color: 'var(--ion-color-dark-contrast)',
-      '& .MuiTypography-root': {
-        marginBottom: '3px'
+      color: "black",
+      "& .MuiTypography-root": {
+        marginBottom: "3px",
       },
-      [theme.breakpoints.up('smPlus')]: {
+      [theme.breakpoints.up("smPlus")]: {
         padding: theme.spacing(2, 1),
       },
     },
     flexSocialMedia: {
-      display: 'flex',
+      display: "flex",
       ...rowGap(10),
       marginTop: theme.spacing(1),
     },
@@ -44,95 +45,94 @@ export const styles = makeStyles((theme: Theme) =>
       // display: 'flex',
       // flexWrap: 'wrap',
       // gap: theme.spacing(2),
-      color: '#999999',
+      color: "black",
       marginTop: theme.spacing(2),
     },
     reserved: {
-      color: '#999999',
+      color: "#999999",
     },
     cookies: {
-      color: '#999999',
+      color: "#999999",
       marginBottom: theme.spacing(2),
-      [theme.breakpoints.up('smPlus')]: {
-        marginBottom: '0',
-      }
+      [theme.breakpoints.up("smPlus")]: {
+        marginBottom: "0",
+      },
     },
     logo: {
-      width: '180px',
-      alignSelf: 'flex-start',
+      width: "180px",
+      alignSelf: "flex-start",
     },
     sunneby: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
       marginTop: theme.spacing(2),
-      [theme.breakpoints.up('smPlus')]: {
-        marginTop: '0',
-      }
-      
+      [theme.breakpoints.up("smPlus")]: {
+        marginTop: "0",
+      },
     },
     about: {
-      color: 'var(--ion-color-dark-contrast)'
+      color: "var(--ion-color-dark-contrast)",
     },
     idpLink: {
-      cursor: 'pointer',
+      cursor: "pointer",
     },
     tiktok: {
-      width: '38px',
+      width: "38px",
     },
     mobile: {
       // display: 'none',
-    [theme.breakpoints.up('smPlus')]: {
-      display: 'initial',
-    }
-  },
-  socialMediaFlex: {
-    display: 'flex',
-    flexDirection: 'row',
-    ...rowGap(5),
-    [theme.breakpoints.up('smPlus')]: {
-      display: 'none',
-    }
-  },
-  socialmediaImage: {
-    width: '30px',
-    [theme.breakpoints.up('smPlus')]: {
-      width: 'initial',
-    }
-  },
-  hereWeAreLink: {
-    // marginTop: '0',
-    [theme.breakpoints.up('smPlus')]: {
-      marginTop: theme.spacing(2),
-    }
-  },
-  getInTouch: {
-    // marginBottom: theme.spacing(2),
-    [theme.breakpoints.up('smPlus')]: {
-      marginBottom: 'initial',
-    }
-  },
-  becomeAPart: {
-    // display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'initial',
-    }
-  },
-  hide: {
-    [theme.breakpoints.up('smPlus')]: {
-      display: 'none',
+      [theme.breakpoints.up("smPlus")]: {
+        display: "initial",
+      },
     },
-    [theme.breakpoints.up('md')]: {
-      display: 'initial',
+    socialMediaFlex: {
+      display: "flex",
+      flexDirection: "row",
+      ...rowGap(5),
+      [theme.breakpoints.up("smPlus")]: {
+        display: "none",
+      },
     },
-  },
+    socialmediaImage: {
+      width: "30px",
+      [theme.breakpoints.up("smPlus")]: {
+        width: "initial",
+      },
+    },
+    hereWeAreLink: {
+      // marginTop: '0',
+      [theme.breakpoints.up("smPlus")]: {
+        marginTop: theme.spacing(2),
+      },
+    },
+    getInTouch: {
+      // marginBottom: theme.spacing(2),
+      [theme.breakpoints.up("smPlus")]: {
+        marginBottom: "initial",
+      },
+    },
+    becomeAPart: {
+      // display: 'none',
+      [theme.breakpoints.up("md")]: {
+        display: "initial",
+      },
+    },
+    hide: {
+      [theme.breakpoints.up("smPlus")]: {
+        display: "none",
+      },
+      [theme.breakpoints.up("md")]: {
+        display: "initial",
+      },
+    },
     hide2: {
-    [theme.breakpoints.up('smPlus')]: {
-      display: 'none',
+      [theme.breakpoints.up("smPlus")]: {
+        display: "none",
+      },
+      [theme.breakpoints.up("mdPlus")]: {
+        display: "initial",
+      },
     },
-    [theme.breakpoints.up('mdPlus')]: {
-      display: 'initial',
-    },
-  }
-  }),
+  })
 );
