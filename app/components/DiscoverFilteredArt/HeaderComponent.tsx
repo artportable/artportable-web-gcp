0b7fd8aa69f82c3c163bbd9b3f02ad959a00fd1b
massiWR <massi.wararoodi@outlook.com>
2025-01-29 16:30:03 +0100

    add headers

--- app/components/DiscoverFilteredArt/HeaderComponent.tsx
+++ app/components/DiscoverFilteredArt/HeaderComponent.tsx
@@ -1,19 +1,9 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination, Scrollbar } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 import { styles } from "./headerComponent.css";
 import { useMediaQuery } from "@material-ui/core";
 import { theme } from "../../../styles/theme";
 import { useEffect, useState } from "react";
-import { KeycloakInstance } from "keycloak-js";
-import { useKeycloak } from "@react-keycloak/ssr";
-import { useRouter } from "next/router";
+
 import { useTranslation } from "next-i18next";
-import Typography from "@mui/material/Typography";
 
 export default function IndexHeroRenewed({ page, filterOpen }) {
   const { t } = useTranslation("index");
@@ -22,22 +12,27 @@ export default function IndexHeroRenewed({ page, filterOpen }) {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          marginTop: "10px",
-          backgroundColor: "#0076D5",
-          height: "250px",
-          width: filterOpen ? "85%" : "100%",
-          marginLeft: "auto",
-        }}
-      >
-        <div style={{ color: "white", marginLeft: "10px" }}>
-          {" "}
-          <h1>Köp det senaste på Artportable</h1>
+      {page === "latest" && (
+        <div className={s.container}>
+          <div style={{ marginLeft: filterOpen ? "25%" : "10%" }}>
+            <div style={{ fontSize: "42px" }} className={s.titleWrapper}>
+              {t("latestTitle")}
+            </div>
+            <div style={{ fontSize: "20px" }}>{t("latestText")}</div>
+          </div>
         </div>
-      </div>
+      )}
+      {page === "likes" && (
+        <div className={s.containerLiked}>
+          <div style={{ marginLeft: filterOpen ? "25%" : "10%" }}>
+            <div style={{ fontSize: "38px" }} className={s.titleWrapper}>
+              {t("discoverTitle")}
+            </div>
+            <br />
+            <div style={{ fontSize: "18px" }}>{t("discoverText")}</div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
