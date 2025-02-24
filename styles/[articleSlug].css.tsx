2745680b1f8dc5e41dea84bf128a69fa94bd2122
massiWR <massi.wararoodi@outlook.com>
2025-02-24 11:12:08 +0100

    add roboto and joan

--- styles/[articleSlug].css.tsx
+++ styles/[articleSlug].css.tsx
@@ -1,103 +1,104 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { rowGap, columnGap } from '../app/utils/styleUtils'
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { rowGap, columnGap } from "../app/utils/styleUtils";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     paper: {
-      maxWidth: '1000px',
-      padding: '20px',
-      justifySelf: 'center',
+      maxWidth: "1000px",
+      padding: "20px",
+      justifySelf: "center",
       zIndex: 1,
-      [theme.breakpoints.up('md')]: {
-        padding: '100px',
-        marginTop: '100px',
+      [theme.breakpoints.up("md")]: {
+        padding: "100px",
+        marginTop: "100px",
       },
     },
     line: {
-      height: '2px',
-      backgroundColor: 'var(--ion-color-primary)',
-      margin: '32px 0',
+      height: "2px",
+      backgroundColor: "var(--ion-color-primary)",
+      margin: "32px 0",
     },
     description: {
-      fontSize: '1.1rem',
+      fontSize: "1.1rem",
       fontWeight: 500,
-      lineHeight: '1.4rem'
+      lineHeight: "1.4rem",
     },
     div: {
-      maxWidth: '100%',
-      justifySelf: 'center',
+      maxWidth: "100%",
+      justifySelf: "center",
       marginTop: theme.spacing(8),
     },
     div2: {
-      maxWidth: '100%',
+      maxWidth: "100%",
     },
     articleImages: {
-      '& figure': {
-        textAlign: 'center',
+      fontFamily: "Joan",
+      "& figure": {
+        textAlign: "center",
       },
-      '& img': {
-        maxWidth: '100%',
-        height: '100%',
+      "& img": {
+        maxWidth: "100%",
+        height: "100%",
       },
-      '& p': {
-        fontSize: '1rem',
+      "& p": {
+        fontSize: "1rem",
       },
-      '& h3': {
+      "& h3": {
         fontWeight: 500,
       },
-      '& a': {
-        textDecoration: 'underline',
+      "& a": {
+        textDecoration: "underline",
       },
-      width: 'fit-content',
+      width: "fit-content",
     },
 
     headingDiv: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
       ...columnGap(16),
     },
 
     authorDiv: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      paddingTop: '32px',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      paddingTop: "32px",
     },
     authorAvatar: {
-      width: '70px',
-      height: '70px',
-      marginBottom: '15px'
+      width: "70px",
+      height: "70px",
+      marginBottom: "15px",
     },
     authorText: {
       fontWeight: 500,
     },
     findArt: {
-      display: 'flex',
-      flexDirection: 'column',
-      marginBottom: '27px',
+      display: "flex",
+      flexDirection: "column",
+      marginBottom: "27px",
       alignItems: "center",
-      [theme.breakpoints.up('sm')]: {
-        flexDirection: 'row',
-        justifyContent: 'center',
+      [theme.breakpoints.up("sm")]: {
+        flexDirection: "row",
+        justifyContent: "center",
       },
     },
     artportable_logo: {
-      margin: '8px 0 0 0',
-      [theme.breakpoints.up('sm')]: {
-        margin: '2px 0 0 10px',
+      margin: "8px 0 0 0",
+      [theme.breakpoints.up("sm")]: {
+        margin: "2px 0 0 10px",
       },
     },
     tagDiv: {
-      display: 'flex',
-      justifyContent: 'center',
+      display: "flex",
+      justifyContent: "center",
       ...rowGap(16),
     },
     background: {
-      position: 'absolute',
+      position: "absolute",
       top: 0,
       left: 0,
-      width: '100%',
-    }
-  }),
+      width: "100%",
+    },
+  })
 );
