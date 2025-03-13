dd43e1e76c457f8d5103ab45d131725585c81434
massiWR <massi.wararoodi@outlook.com>
2025-03-13 15:55:18 +0100

    remove facebook pixl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -148,22 +148,7 @@ function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
             `,
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '331292174363133');
-        fbq('track', 'PageView');
-      `,
-          }}
-        />
+
         <noscript>
           <img
             height="1"
