60f7d1486d4443c3dc5b7794ea804844d282fd78
massiWR <massi.wararoodi@outlook.com>
2025-04-07 15:57:38 +0200

    add news

--- app/components/DiscoverStoriesTab/discoverStoriesTab.css.tsx
+++ app/components/DiscoverStoriesTab/discoverStoriesTab.css.tsx
@@ -1,15 +1,17 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const styles = makeStyles((theme: Theme) =>
   createStyles({
     btnContainer: {
-      display: 'flex', 
-      alignItems: 'center', 
-      margin: '20px', 
-      flexDirection: 'column'
+      display: "flex",
+      alignItems: "center",
+      margin: "20px",
+      flexDirection: "column",
     },
     btn: {
-      fontSize:'20px',
-    }
-  }),
+      fontSize: "20px",
+      backgroundColor: "black",
+      borderRadius: "30px",
+    },
+  })
 );
