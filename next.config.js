df23ef19c91d72081d3300d4eff0e22a0f98b282
massiWR <massi.wararoodi@outlook.com>
2025-05-18 22:09:22 +0200

    opti

--- next.config.js
+++ next.config.js
@@ -1,41 +1,29 @@
 const { i18n } = require("./next-i18next.config");
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-// const signUpRedirect = process.env.REDIRECT_TO_SIGN_UP
+
 module.exports = withBundleAnalyzer({
+  swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Fix to reload page after upgrade account
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/register',
-  //       destination: signUpRedirect,
-  //       permanent: true,
-  //     },
-
-  //   ]
-  // },
-
   images: {
-    domains: [
-      "artportabletest.blob.core.windows.net",
-      "artportableprod.blob.core.windows.net",
-    ],
+    domains:
+      process.env.NODE_ENV === "production"
+        ? [
+            "artportabletest.blob.core.windows.net",
+            "artportableprod.blob.core.windows.net",
+          ]
+        : [],
     loader: "default",
   },
   i18n,
   async rewrites() {
     return [
       {
-        // match anything that does not contain @ and redirect to 404
-        // TODO: Fix so that only valid characters work and that the @ is missing at the start of the string
-        // https://github.com/vercel/next.js/issues/23741
         source: "/profile/:username([^@]{1,}$)",
-        destination: "/404", // Matched parameters can be used in the destination /[^@]{1,}$/
+        destination: "/404",
       },
       {
         source: "/sv/:path*",
