a74f88a119becbc966dd202e1d20dfdefc5f4cfe
massiWR <massi.wararoodi@outlook.com>
2025-05-22 08:55:09 +0200

    fix staging

--- app/components/SearchField/SearchField.tsx
+++ app/components/SearchField/SearchField.tsx
@@ -94,8 +94,11 @@ const SearchField = ({ onFilter, searchQuery, iconOnly = false }) => {
       );
     }, 3000); // Change every 3 seconds
 
-    return () => clearInterval(interval);
-  }, []);
+    // Cleanup function to clear the interval when component unmounts
+    return () => {
+      clearInterval(interval);
+    };
+  }, [rotatingItems]); // Add rotatingItems to dependency array
 
   const handleInputChange = (event) => {
     const value = event.target.value.trim().toLowerCase();
